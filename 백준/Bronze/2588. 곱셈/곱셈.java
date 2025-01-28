f2d2da37e9574fe09e51df1f35a4a3dcd0526055
brilliant13 <140619565+brilliant13@users.noreply.github.com>
2025-01-28 23:06:55 +0900

    String클래스, charAt()

--- 백준/Bronze/2588. 곱셈/곱셈.java
+++ 백준/Bronze/2588. 곱셈/곱셈.java
@@ -2,14 +2,16 @@ import java.util.*;
 public class Main{
     public static void main(String[]args){
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int m = sc.nextInt();
-        int r3=n*(m%10);
-        int r4=n*((m/10)%10);
-        int r5=n*(m/100);
-        System.out.println(r3);
-        System.out.println(r4);
-        System.out.println(r5);
-        System.out.println(r3+r4*10+r5*100);
+        int A = sc.nextInt();
+        String B = sc.next();
+
+        sc.close();
+
+        System.out.println(A*(B.charAt(2) - '0'));
+        System.out.println(A*(B.charAt(1) - '0'));
+        System.out.println(A*(B.charAt(0) - '0'));
+        System.out.println(A*Integer.parseInt(B));
+
+
     }
 }
