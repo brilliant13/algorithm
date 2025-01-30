81d661f132665c9555a99b0875d68f9e2f52aa6f
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-01-30 22:11:51 +0900

    Update 주사위 세개.java

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -6,28 +6,13 @@ public class Main {
         int n = sc.nextInt();
         int m = sc.nextInt();
         int l = sc.nextInt();
-        int price =0;
-        int max =0;
-
-        //같은 눈 3개
-        if (n == m && m == l) {
-            price = 10000+n*1000;
-        }
-        //같은 눈 2개
-        else if (n == m || n == l || m == l) {
-            if (n == m || n == l) {
-                price = 1000 + n * 100;
-            } else {
-                price = 1000 + m*100;
-            }
-        }
-        //모두 다른 눈
-        else{
-            int max1 = Math.max(n, m);
-            max = Math.max(max1, l);
-            price = max*100;
-        }
-        System.out.println(price);
-
+        if (n == m && m == l)
+            System.out.println(10000 + n * 1000);
+        else if(n==m || n==l)
+            System.out.println(1000+n*100);
+        else if(m==l)
+            System.out.println(1000+m*100);
+        else
+            System.out.println(Math.max(Math.max(n, m), l)*100);
     }
 }
