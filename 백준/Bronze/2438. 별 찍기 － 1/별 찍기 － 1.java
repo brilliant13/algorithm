bcf20fb9cf2aaaef64f536bd6c9f08b6f77a54bb
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-01-30 22:27:41 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 108 ms, Memory: 14156 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -1,14 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        for(int i=0;i<n;i++){
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        br.close();
+
+        StringBuilder sb = new StringBuilder();
+
+        for(int i=0;i<N;i++){
             for (int j = 0; j <= i; j++) {
-                System.out.print("*");
+                sb.append('*');
             }
-            System.out.println();
+            sb.append('\n');
         }
+        System.out.print(sb);
     }
 }
