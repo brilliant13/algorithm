84ae41b2b06578fc8f06262bf83508e2e6ef21bb
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-01-31 00:53:07 +0900

    [Bronze V] Title: 개수 세기, Time: 108 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/10807. 개수 세기/개수 세기.java
+++ 백준/Bronze/10807. 개수 세기/개수 세기.java
@@ -5,23 +5,27 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int count =0;
-        int N = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int N = Integer.parseInt(br.readLine());
+        int count = 0;
         int[] arr = new int[N];
 
-        for (int i = 0; i < N; i++) {
-            arr[i] = sc.nextInt();
-        }
-        int b = sc.nextInt();
 
+        StringTokenizer st;
+        st = new StringTokenizer(br.readLine(), " ");
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+        int x = Integer.parseInt(br.readLine());
         for (int j = 0; j < arr.length; j++) {
-            if (b == arr[j]) {
+            if (x == arr[j]) {
                 count++;
             }
         }
         System.out.println(count);
-
 
     }
 }
