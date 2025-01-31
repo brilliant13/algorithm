6c52c14274aa96bd7711754162ee95ec628747e9
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-01 00:13:59 +0900

    [Bronze V] Title: X보다 작은 수, Time: 180 ms, Memory: 17392 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.java
@@ -4,30 +4,28 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        //입력 N, X
+        // 입력 처리
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        
+
+        // 첫 번째 줄 입력 (N, X)
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int X = Integer.parseInt(st.nextToken());
-        //입력 수열 A
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        int arr[] = new int[st2.countTokens()];
-        for (int i = 0; i < arr.length; i++) {
-            arr[i] = Integer.parseInt(st2.nextToken());
-        }
-        //X 탐색
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] < X) {
-                bw.write(arr[i] + " ");
+
+        // 두 번째 줄 입력 (수열 A)
+        st = new StringTokenizer(br.readLine()); //  같은 변수 `st`를 재사용
+
+        //배열 사용하지 않고 바로 출력 (메모리절약)
+        while (st.hasMoreTokens()) {
+            int num = Integer.parseInt(st.nextToken());
+            if (num < X) {
+                bw.write(num+" ");
             }
         }
         br.close();
         bw.flush();
         bw.close();
-
-
     }
 }
 
