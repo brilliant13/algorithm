7c65dd49652980b2b9036c65ca4bae36188a076c
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-01-31 19:31:02 +0900

    [Bronze V] Title: 단어 길이 재기, Time: 104 ms, Memory: 14268 KB -BaekjoonHub

---
+++ 백준/Bronze/2743. 단어 길이 재기/단어 길이 재기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = "";
+        str = br.readLine();
+        char[] ch = str.toCharArray();
+        System.out.println(ch.length);
+        
+    }
+}
+
