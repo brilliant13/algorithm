870970ba8bfd801719c9e62c8760df7cb7cc10ee
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-01-31 22:08:16 +0900

    [Bronze V] Title: 검증수, Time: 100 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Bronze/2475. 검증수/검증수.java
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -1,28 +1,18 @@
-
-
 import java.io.*;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static int f(int [] arr) {
-        int sum =0;
-        for (int i = 0; i < arr.length; i++) {
 
-            sum += arr[i]*arr[i];
-        }
-        return sum%10;
-    }
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-        int N = st.countTokens();
-        int arr[] = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
-        }
-        System.out.println(f(arr));
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
+        int total =0;
+        while (st.hasMoreTokens()) {
+            int a = Integer.parseInt(st.nextToken());
+            total +=a*a;
+        }
+        System.out.println(total % 10);
     }
 }
 
