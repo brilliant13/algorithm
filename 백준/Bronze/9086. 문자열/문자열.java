4f03f969a7355472b85a97fa52cd79c653f66285
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-01-31 21:10:02 +0900

    [Bronze V] Title: 문자열, Time: 116 ms, Memory: 15992 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,20 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int T = Integer.parseInt(br.readLine());
+        String str = "";
+        for (int i = 0; i < T; i++) {
+            str = br.readLine();
+            bw.write(str.charAt(0)+"" + str.charAt(str.length() - 1) + "\n");
+
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+}
+
