8fed1b8a11026e4419b02a67a4c6eee8c10d52e2
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-01 01:06:59 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 176 ms, Memory: 17848 KB -BaekjoonHub

--- 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -1,49 +1,32 @@
-
-
 import java.io.*;
-import java.util.StringTokenizer;
+import java.util.*;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        //입력: 행렬의 크기 N ,M
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
-        //입력: NxM 행렬 입력
-        int[][] A = new int[N][M];
-        int[][] B = new int[N][M];
-//        for (int i = 0; i < 2*N; i++) {
-//            st = new StringTokenizer(br.readLine());
-//        }
-        for (int i = 0; i < N; i++) {
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                A[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        for (int i = 0; i < N; i++) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < M; j++) {
-                B[i][j] = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < m; j++) {
+                sb.append(arr[i][j] + Integer.parseInt(st.nextToken()) + " ");
             }
+            sb.append("\n");
         }
-        //행렬 덧셈
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                bw.write((A[i][j] + B[i][j])+" ");
-            }
-            bw.newLine();
-        }
-        br.close();
-        bw.flush();
-        bw.close();
+        System.out.println(sb);
+
+
+
 
     }
+
 }
-
-
-
