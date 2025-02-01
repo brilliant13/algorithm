8a792ecac572f2d91cd41ae75f41ad4d9460147f
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-01 09:05:26 +0900

    [Bronze V] Title: N 찍기, Time: 236 ms, Memory: 22388 KB -BaekjoonHub

--- 백준/Bronze/2741. N 찍기/N 찍기.java
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -5,15 +5,17 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
         int N = Integer.parseInt(br.readLine());
-        for (int i = 0; i < N; i++) {
-            bw.write(i+1 + "\n");
-        }
-        bw.flush();
-        bw.close();
         br.close();
+        int i=1;
+        StringBuilder sb = new StringBuilder();
+
+        while (i <= N) {
+            sb.append(i+"\n");
+            i++;
+        }
+        System.out.println(sb);
     }
 
 }
