18eb2053e2373a084b69d0eb11ceaf3f0ba4004d
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-01 14:49:47 +0900

    [Bronze V] Title: 등록, Time: 92 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/7287. 등록/등록.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(47);
+        System.out.println("strongdev77");
+    }
+}
