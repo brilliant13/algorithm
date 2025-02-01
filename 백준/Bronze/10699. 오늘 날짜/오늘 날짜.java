e17881fd9992c462d82d38fb891441ad5041e2c7
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-01 14:47:35 +0900

    [Bronze V] Title: 오늘 날짜, Time: 160 ms, Memory: 17288 KB -BaekjoonHub

---
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.java
@@ -0,0 +1,10 @@
+import java.io.*;
+import java.time.LocalDate;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        LocalDate now = LocalDate.now();
+        System.out.println(now);
+    }
+}
