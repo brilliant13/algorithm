27ea4526a0078d3ee950b8762937ba8ccead7d7b
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-04 13:17:42 +0900

    [Bronze II] Title: 수 정렬하기, Time: 112 ms, Memory: 14332 KB -BaekjoonHub

--- 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.java
@@ -1,26 +1,29 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-
+import java.io.IOException;
+ 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
-        int[] arr = new int[N];
-
-        for(int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(br.readLine());
-        }
-
-        //Arrays.sort 정렬 메소드
-        Arrays.sort(arr);
-
-        for (int val : arr) {
-            sb.append(val+"\n");
-        }
-        System.out.println(sb);
-
-    }
+	public static void main(String[] args) throws IOException {
+    
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringBuilder sb = new StringBuilder();
+        
+		int N = Integer.parseInt(br.readLine());
+        
+		boolean[] arr = new boolean[2001];
+		
+		for(int i = 0; i < N; i++) {
+			arr[Integer.parseInt(br.readLine()) + 1000] = true;
+		}
+ 
+		// 정렬 과정이 따로 필요 없음
+		
+		for(int i = 0; i < 2001; i++) {
+			if(arr[i]) {
+				sb.append(i - 1000).append('\n');
+			}
+		}
+		System.out.println(sb);
+	}
 }
