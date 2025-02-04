668726bfe4164ad7e8a76067a6c69b4f3d9742af
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-05 00:19:24 +0900

    [Bronze II] Title: 소수 찾기, Time: 100 ms, Memory: 14216 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.java
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.java
@@ -8,21 +8,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int count = 0;
-
+        int[] numbers = new int[N];
+        int max =0;
         for (int i = 0; i < N; i++) {
-            int x = Integer.parseInt(st.nextToken());
-            if (x == 1) continue;
-
-            boolean isPrime = true;
-            for (int j = 2; j <= Math.sqrt(x); j++) {
-                if (x % j == 0) {
-                    isPrime =false;
-                    break;
-                }
+            numbers[i] = Integer.parseInt(st.nextToken());
+            if(numbers[i] > max) {
+                max = numbers[i];
             }
-            if(isPrime) count++;
+        }
+
+        boolean isComposite[] = make_prime(max);
+
+        int count =0;
+        for (int num : numbers) {
+            if (num > 1 && !isComposite[num]) {
+                count++;
+            }
         }
         System.out.println(count);
+    }
+    public static boolean[] make_prime(int max) {
+        boolean[] prime = new boolean[max + 1];
+        if(max >= 0) prime[0] = true;
+        if(max >= 1) prime[1] = true;
+
+        for (int i = 2; i <= Math.sqrt(max); i++) {
+            if (!prime[i]) { //소수라면, 그것의 배수는 합성수처리한다. 합성수는 소수의 반대.
+                for (int j = i * i; j < max + 1; j += i) {
+                    prime[j] = true;
+                }
+            }
+        }
+        return prime;
     }
 }
