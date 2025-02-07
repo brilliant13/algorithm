0e8d4f882128a456ef6d60a7254704320db8129e
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-07 11:12:49 +0900

    [Bronze I] Title: 팰린드롬수, Time: 104 ms, Memory: 14064 KB -BaekjoonHub

---
+++ 백준/Bronze/1259. 팰린드롬수/팰린드롬수.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        while (true) {
+            String input = br.readLine();
+            if (input.equals("0")) {
+                break;
+            }
+            bw.write(check(input));
+        }
+        br.close();
+        bw.flush();
+        bw.close();
+    }
+
+    static String check(String s) {
+        int length = s.length();
+        int half = length/2;
+
+        if (length % 2 == 0) {
+            for (int i = 0; i < half; i++) {
+                if (s.charAt(i) != s.charAt(length - 1 - i)) return "no\n";
+            }
+        } else {
+            for (int j = 0; j < half + 1; j++) {
+                if (s.charAt(j) != s.charAt(length - 1 - j)) return "no\n";
+            }
+        }
+        return "yes\n";
+    }
+}
