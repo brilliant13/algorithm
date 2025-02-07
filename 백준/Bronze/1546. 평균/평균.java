1c8e2294578f12c771b9c11d96b784a2dfbbc2cf
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-07 11:27:23 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14312 KB -BaekjoonHub

---
+++ 백준/Bronze/1546. 평균/평균.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int arr[] = new int[N];
+        double max = 0;
+        double average =0;
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] > max) max = arr[i];
+        }
+        for (int i = 0; i < N; i++) {
+            average += arr[i] / max * 100;
+        }
+        average /= N;
+        System.out.println(average);
+
+    }
+
+
+}
