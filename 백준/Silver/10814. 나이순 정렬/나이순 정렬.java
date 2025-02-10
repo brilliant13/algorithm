ce6a2e90dc7f9c7ace0e5b27ac0b512cc239d912
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-10 12:37:53 +0900

    [Silver V] Title: 나이순 정렬, Time: 380 ms, Memory: 41812 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.java
@@ -1,51 +1,33 @@
 import java.io.*;
-import java.util.Arrays;
-import java.util.Comparator;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
 
-        Person[] p = new Person[T];
+        int N = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < T; i++) {
-            StringTokenizer st = new StringTokenizer(br.readLine());
+        // 입력되는 나이의 범위 : 1 ~ 200
+        StringBuilder[] p = new StringBuilder[201];
+        //객체배열의 인덱스에 각 StringBuilder 객체를 생성해준다.
+        for (int i = 0; i < p.length; i++) {
+            p[i] = new StringBuilder();
+        }
+
+
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
             int age = Integer.parseInt(st.nextToken());
             String name = st.nextToken();
-            p[i] = new Person(age, name);
+            // 카운팅 정렬 : 나이를 index 로 하여 해당 배열에 나이와 이름을 append() 한다
+            p[age].append(age).append(' ').append(name).append('\n');
         }
-
-        //Arrays.sort는 Stable sort. 입력순서는 유지된다.
-        Arrays.sort(p, new Comparator<Person>() {
-            @Override
-            public int compare(Person s1, Person s2) {
-                return s1.age - s2.age;
-            }
-        });
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < T; i++) {
-            sb.append(p[i]);
+        for (StringBuilder val : p) {
+            sb.append(val);
         }
         System.out.println(sb);
-
     }
 
-    private static class Person {
-        int age;
-        String name;
-
-        public Person(int age, String name) {
-            this.age = age;
-            this.name = name;
-        }
-
-        @Override
-        public String toString() {
-            return age + " " + name + "\n";
-        }
-
-    }
 }
