13441df85944f448d6293bebae893e4caa9809b2
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-13 11:18:54 +0900

    [Bronze II] Title: 벌집, Time: 104 ms, Memory: 14148 KB -BaekjoonHub

---
+++ 백준/Bronze/2292. 벌집/벌집.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int count =1;
+        int range =2;
+        if (N == 1) {
+            System.out.print(1);
+        } else {
+            while (range <= N) {
+                range = range + (6 * count);
+                count++;
+            }
+            System.out.println(count);
+        }
+
+
+    }
+
+
+}
