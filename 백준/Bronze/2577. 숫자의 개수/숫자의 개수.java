4116dcc7babc3826abdd5277e73b3d6c8d22c72b
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-14 11:58:10 +0900

    [Bronze II] Title: 숫자의 개수, Time: 104 ms, Memory: 14052 KB -BaekjoonHub

--- 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
+++ 백준/Bronze/2577. 숫자의 개수/숫자의 개수.java
@@ -9,16 +9,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
         int[] arr = new int[10];
 
-        int val = Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine());
-        String str = String.valueOf(val);
-        for (int i = 0; i < str.length(); i++) {
-            arr[(str.charAt(i)-48)]++;
+        while (val != 0) {
+            arr[val % 10]++;
+            val/=10;
         }
         for (int v : arr) {
             System.out.println(v);
         }
+
         }
     }
 
