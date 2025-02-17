c08465b1be188ef3502e25f330313009eaba8954
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-17 12:24:23 +0900

    [Silver V] Title: 카드1, Time: 108 ms, Memory: 14212 KB -BaekjoonHub

--- 백준/Silver/2161. 카드1/카드1.java
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -4,25 +4,28 @@ import java.util.Queue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        Queue<Integer> q = new LinkedList<>();
-
         int N = Integer.parseInt(br.readLine());
-
-        for (int i = 1; i <= N; i++) {
-            q.offer(i);
-        }
         StringBuilder sb = new StringBuilder();
-        while (q.size() > 1) {
-            sb.append(q.poll()).append(" ");
-            q.offer(q.poll());
+
+
+        int[] q = new int[2 * N];
+        for(int i = 1; i <= N; i++) {
+            q[i] = i;
         }
-        sb.append(q.poll()).append(" ");
+        int prev_index = 1;
+        int last_index = N;
 
+        while(N-- > 1) {
+            sb.append(q[prev_index]).append(" ");
+            prev_index++;
+            q[last_index + 1] = q[prev_index];
+            last_index++;
+            prev_index++;
+        }
+        sb.append(q[prev_index]).append(" ");
         System.out.println(sb);
-
 
     }
 
