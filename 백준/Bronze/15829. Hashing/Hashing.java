4f20b751bbcb6cdfc6a3b77df0faf2439027a41f
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-02-18 20:36:50 +0900

    [Bronze II] Title: Hashing, Time: 100 ms, Memory: 14156 KB, Score: 50 point -BaekjoonHub

---
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int L = Integer.parseInt(br.readLine());
+        char[] chars = new char[L];
+
+        String str = br.readLine();
+        for (int i = 0; i < str.length(); i++) {
+            chars[i]=str.charAt(i);
+        }
+        System.out.println(hash(chars));
+
+    }
+
+    static long hash(char[] chars) {
+        long r = 31;
+        long M = 1234567891;
+        long power = 1;
+        long result =0;
+        for (int i = 0; i < chars.length; i++) {
+            result += (chars[i]-'a'+1)*power;
+            power*=r;
+        }
+        return result%M;
+    }
+
+}
