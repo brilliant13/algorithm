f097b5f6ca8afc1aaa5d29b62c3dc12cc123d6e1
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-03-07 20:00:39 +0900

    [Bronze II] Title: 상수, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Bronze/2908. 상수/상수.java
+++ 백준/Bronze/2908. 상수/상수.java
@@ -1,20 +1,13 @@
-import java.util.Scanner;
- 
+import java.io.*;
+import java.util.StringTokenizer;
+
 public class Main {
- 
-	public static void main(String[] args) {
- 
-		Scanner in = new Scanner(System.in);
-        
-		int A = in.nextInt();
-		int B = in.nextInt();
-        
-        in.close();
-        
-		A = Integer.parseInt(new StringBuilder().append(A).reverse().toString());
-		B = Integer.parseInt(new StringBuilder().append(B).reverse().toString());
-		
-		System.out.print(A > B ? A : B);
-	
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+        int A = Integer.parseInt(new StringBuilder().append(st.nextToken()).reverse().toString());
+        int B = Integer.parseInt(new StringBuilder().append(st.nextToken()).reverse().toString());
+        System.out.println(Math.max(A, B));
+    }
 }
