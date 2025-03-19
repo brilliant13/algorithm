a203a0df4728cbf2f376d5b75a218d668c927948
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-03-19 20:46:33 +0900

    [Silver V] Title: 공포의 면담실, Time: 536 ms, Memory: 83940 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
+++ 백준/Silver/30088. 공포의 면담실/공포의 면담실.java
@@ -1,39 +1,46 @@
-import java.io.*;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine());
-        StringTokenizer st;
-        ArrayList<ArrayList<Integer>> list = new ArrayList<>();
-        int[] sum = new int[N+1];
-        int end=0;
-        for (int i = 0; i < N; i++) {
-            list.add(new ArrayList<Integer>());
-        }
 
+    static final BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static final StringBuilder sb = new StringBuilder();
+    static StringTokenizer st;
+
+    int N;
+    int M;
+    int[] sum;
+    long res;
+
+    private void solution() throws IOException {
+
+        N = Integer.parseInt(br.readLine());
+        sum = new int[N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            int people = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < people; j++) {
-                int value =Integer.parseInt(st.nextToken());
-                list.get(i).add(value);
-                sum[i+1] += value;
+            M = Integer.parseInt(st.nextToken());
+            for (int j = 0; j < M; j++) {
+                sum[i] += Integer.parseInt(st.nextToken());
             }
         }
-        Arrays.sort(sum);
-        int count = N;
-        for (int i = 1; i < N + 1; i++) {
-            for (int j = count; j > 0; j--) {
-                end += sum[i];
-            }
-            count --;
-        }
-        System.out.println(end);
 
+        Arrays.sort(sum);
+        long prefixSum = 0;
+        res = 0;
+        for (int i = 0; i < N; i++) {
+            prefixSum += sum[i];
+            res += prefixSum;
+        }
+
+        System.out.println(res);
     }
 
+    public static void main(String[] args) throws IOException {
+
+        Main main = new Main();
+        main.solution();
+    }
 }
