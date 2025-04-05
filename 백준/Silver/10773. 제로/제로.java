1c8ebd389d33e1fc6fa25fd6974eb422a92dd289
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-04-05 13:49:58 +0900

    [Silver IV] Title: 제로, Time: 192 ms, Memory: 23716 KB -BaekjoonHub

---
+++ 백준/Silver/10773. 제로/제로.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        Stack<Integer> stack = new Stack<>();
+        int K = Integer.parseInt(br.readLine());
+        for (int i = 0; i < K; i++) {
+            int n = Integer.parseInt(br.readLine());
+            if (n == 0) {
+                stack.pop();
+            } else {
+                stack.push(n);
+            }
+        }
+        int sum =0;
+        for (int x : stack) {
+            sum+=x;
+        }
+        System.out.print(sum);
+    }
+}
+
