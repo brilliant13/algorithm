0d1e7d0bdd6b97a9bbe355b825d5ee5fe0f094ec
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-05-01 18:01:56 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 27376 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 수열 원소 개수 입력
+        int n = Integer.parseInt(br.readLine());
+        // 1 <=   <= n 원소 입력
+        int max = 1; //뽑힌 수열의 최댓값 +1
+        int target =0;
+        Stack<Integer> stack = new Stack<>(); //스택 자료구조
+        StringBuilder sb = new StringBuilder();
+        boolean isPossible = true; // 수열 생성 가능 여부
+
+
+        for (int i = 0; i < n; i++) {
+            target = Integer.parseInt(br.readLine());
+            if (target >= max) {
+                //타겟이 현재 max보다 큰 경우
+                for (int j = max; j <= target; j++) {
+                    stack.push(j);
+                    sb.append("+\n");
+                }
+                max = target+1;
+                stack.pop();
+                sb.append("-\n");
+            } else {
+                //타겟이 현재 max보다 작은 경우
+                if (target == stack.peek()) {
+                    stack.pop();
+                    sb.append("-\n");
+                } else {
+                    isPossible = false;
+                    break;
+                }
+            }
+
+        }
+        // 수열 생성 가능 여부에 따른 출력
+        if (isPossible) {
+            System.out.println(sb);
+        } else {
+            System.out.println("NO");
+        }
+    }
+}
+
