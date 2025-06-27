1a7e41d1b2a2974468d29459470f66ab04d5ce4d
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-06-27 18:51:28 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 1, Time: 16.98 ms, Memory: 171 MB -BaekjoonHub

--- 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
+++ 프로그래머스/0/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.java
@@ -1,12 +1,14 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] arr) {
-        for(int i=0; i<arr.length; i++){
-            if(arr[i]>=50 && arr[i]%2 ==0){
-                arr[i] /= 2;
-            } else if (arr[i]<50 && arr[i]%2 !=0){
-                arr[i] *=2;
-            }
-        }
-        return arr;
+      return Arrays.stream(arr)
+                    .map(operand ->
+                        operand >=50 && operand % 2 ==0
+                        ? operand /2
+                        : operand <50 && operand % 2 ==1
+                        ? operand *2
+                        : operand)
+                    .toArray();
     }
 }
