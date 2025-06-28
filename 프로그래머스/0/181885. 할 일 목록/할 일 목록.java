6bf2fcacc5f521227f7792b60653b91172734a8e
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-06-28 21:24:31 +0900

    [level 0] Title: 할 일 목록, Time: 11.91 ms, Memory: 95.7 MB -BaekjoonHub

--- 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
+++ 프로그래머스/0/181885. 할 일 목록/할 일 목록.java
@@ -1,23 +1,10 @@
 class Solution {
     public String[] solution(String[] todo_list, boolean[] finished) {
-        int count = 0;
-        for (int i=0 ; i<finished.length; i++){
-            if(!finished[i]){
-                 count++;
-            }
+        String str = "";
+        for(int i=0; i<finished.length; i++){
+            str = !finished[i] ? str + todo_list[i]+"," : str;
         }
-        System.out.println("마치치 못한 개수 = "+count);
-
-        String[] answer = new String[count];
-
-        int idx =0;
-        for (int i=0 ; i<finished.length; i++){
-            if(!finished[i]){
-                 answer[idx] = todo_list[i];
-                System.out.println("마치치 못한 일 = "+ todo_list[i]);
-                 idx++;
-            }
-        }
-        return answer;
+        
+        return str.split(",");
     }
 }
