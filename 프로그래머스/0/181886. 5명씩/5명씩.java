72658393ad56da2c5ac027478d657d9b3ef7289f
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-01 15:26:47 +0900

    [level 0] Title: 5명씩, Time: 0.04 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/0/181886. 5명씩/5명씩.java
+++ 프로그래머스/0/181886. 5명씩/5명씩.java
@@ -1,11 +1,13 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public String[] solution(String[] names) {
-        int idx =0;
-        String[] answer = new String[names.length % 5 == 0 ? names.length/5 : names.length/5 +1];
-        for (int i=0; i<names.length; i+=5){
-            answer[idx++] = names[i];
-        }
-        return answer;
-        
+       List<String> result = new ArrayList<>();
+       for(int i=0; i< names.length; i+=5){
+           result.add(names[i]);
+       }
+    
+        return result.toArray(new String[0]);
     }
 }
