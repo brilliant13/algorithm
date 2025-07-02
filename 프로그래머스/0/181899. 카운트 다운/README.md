85a4e438914524f8ffac88424578f5352061da16
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-02 12:23:17 +0900

    [level 0] Title: 카운트 다운, Time: 4.18 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/0/181899. 카운트 다운/README.md
+++ 프로그래머스/0/181899. 카운트 다운/README.md
@@ -1,10 +1,10 @@
 # [level 0] 카운트 다운 - 181899 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181899) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181899?language=java) 
 
 ### 성능 요약
 
-메모리: 84.2 MB, 시간: 0.03 ms
+메모리: 76.9 MB, 시간: 4.18 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 12:19:48
+2025년 07월 02일 12:23:15
 
 ### 문제 설명
 
--- 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
+++ 프로그래머스/0/181899. 카운트 다운/카운트 다운.java
@@ -1,10 +1,6 @@
+import java.util.stream.*;
 class Solution {
     public int[] solution(int start_num, int end_num) {
-        int size = start_num - end_num +1;       
-        int[] answer = new int[size];
-        for(int i=0 ; i<size; i++){
-            answer[i] = start_num - i;
-        }
-        return answer;
+        return IntStream.rangeClosed(-start_num,-end_num).map(i->-i).toArray();
     }
 }
