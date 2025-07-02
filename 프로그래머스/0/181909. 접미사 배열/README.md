680b0ff3b3fa50562ccaf9b9ef474adab025434c
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-02 13:52:25 +0900

    [level 0] Title: 접미사 배열, Time: 5.05 ms, Memory: 89.7 MB -BaekjoonHub

--- 프로그래머스/0/181909. 접미사 배열/README.md
+++ 프로그래머스/0/181909. 접미사 배열/README.md
@@ -1,10 +1,10 @@
 # [level 0] 접미사 배열 - 181909 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181909?language=java) 
 
 ### 성능 요약
 
-메모리: 87.8 MB, 시간: 2.22 ms
+메모리: 89.7 MB, 시간: 5.05 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 02일 13:34:47
+2025년 07월 02일 13:52:23
 
 ### 문제 설명
 
--- 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
+++ 프로그래머스/0/181909. 접미사 배열/접미사 배열.java
@@ -1,16 +1,10 @@
-import java.util.Collections;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.stream.Stream;
+import java.util.stream.IntStream;
 
 class Solution {
     public String[] solution(String my_string) {
-        List <String> list = new ArrayList<>();
-        for(int i=0; i<my_string.length(); i++){
-            list.add(my_string.substring(my_string.length()-1-i,my_string.length()));
-        }        
-        Collections.sort(list); // List<String> ->String[]
-        String[] result = list.stream().toArray(String[]::new);
-        return result;
+        return IntStream.range(0, my_string.length()) 
+                 .mapToObj(my_string::substring)
+                 .sorted()
+                 .toArray(String[]::new);
     }
 }
