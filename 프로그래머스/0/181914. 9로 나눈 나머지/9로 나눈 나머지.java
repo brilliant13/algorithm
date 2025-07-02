e65f3ae996cf0b9df5d92f78db3164d5700a32e2
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-02 14:21:43 +0900

    [level 0] Title: 9로 나눈 나머지, Time: 10.49 ms, Memory: 80 MB -BaekjoonHub

--- 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/0/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -1,9 +1,5 @@
 class Solution {
     public int solution(String number) {
-        int sum = 0;
-        for (char each : number.toCharArray()){
-            sum += Integer.parseInt(""+each);
-        }
-        return sum%9;
+        return number.chars().map(c->(c-'0')%9).sum()%9;
     }
 }
