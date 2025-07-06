8fe5a3e74779a6123156da153a44dc7eea923eec
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-06 21:07:03 +0900

    [level 0] Title: 콜라츠 수열 만들기, Time: 14.33 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/0/181919. 콜라츠 수열 만들기/README.md
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 콜라츠 수열 만들기 - 181919 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181919) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181919?language=java) 
 
 ### 성능 요약
 
-메모리: 72.3 MB, 시간: 2.88 ms
+메모리: 76.3 MB, 시간: 14.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 06일 19:24:40
+2025년 07월 06일 21:07:01
 
 ### 문제 설명
 
--- 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
+++ 프로그래머스/0/181919. 콜라츠 수열 만들기/콜라츠 수열 만들기.java
@@ -1,22 +1,9 @@
-import java.util.ArrayList;
-import java.util.List;
-
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int n) {
-        List<Integer> list = new ArrayList<>();
-        list.add(n);
-        while(n!=1){
-            if(n%2==0) n /=2;
-            else n = 3*n+1;
-            list.add(n);
-        }
-        int [] answer = list.stream().mapToInt(Integer::intValue).toArray();
-            
-        //Arrays.stream(arr)
-        // List.stream().mapToInt(Integer::intValue).toArray();
-        //Stream<Integer> -> IntStream -> toArray()
-            
-            
-        return answer;
+        return IntStream.concat(
+                        IntStream.iterate(n,i->i>1,i->i%2 ==0 ? i/2 : 3*i +1 ),
+                        IntStream.of(1))
+                .toArray();
     }
 }
