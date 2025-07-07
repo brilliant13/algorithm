2ca75db4bdfd4af52d3b1d66782be807a0038249
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-07 18:30:27 +0900

    [level 0] Title: 마지막 두 원소, Time: 5.12 ms, Memory: 86.7 MB -BaekjoonHub

--- 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/0/181927. 마지막 두 원소/마지막 두 원소.java
@@ -6,13 +6,14 @@ import java.util.stream.IntStream;
 
 class Solution {
     public int[] solution(int[] num_list) {
-        List<Integer> list = new ArrayList<>();
-        if(num_list[num_list.length -1 ]> num_list[num_list.length -2]){
-            list.add(num_list[num_list.length -1 ] - num_list[num_list.length -2]);
-        }else{
-            list.add(num_list[num_list.length -1 ]*2);
-        }
-        IntStream is = list.stream().mapToInt(Integer::intValue);
-        return IntStream.concat(Arrays.stream(num_list) ,is).toArray();
+        int last = num_list[num_list.length -1];
+        int secondLast = num_list[num_list.length -2];
+        
+        int newValue = last > secondLast ? last - secondLast : last*2;
+        
+        return IntStream.concat(
+                        Arrays.stream(num_list), IntStream.of(newValue)
+        ).toArray();
+        
     }
 }
