41465b98dd23d670821da7ec1be1e2cf08d2ffe2
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-08 11:45:57 +0900

    [level 0] Title: A 강조하기, Time: 0.03 ms, Memory: 81.7 MB -BaekjoonHub

--- 프로그래머스/0/181874. A 강조하기/A 강조하기.java
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.java
@@ -1,15 +1,7 @@
 class Solution {
     public String solution(String myString) {
-        String answer = "";
-        
-        char [] arr = myString.toCharArray();
-        for (int i=0; i<arr.length; i++){
-            if(arr[i] == 'a') arr[i] -= 32;
-            else if(arr[i] != 'A') arr[i] = Character.toLowerCase(arr[i]);
-        }
-        answer = String.valueOf(arr);
-        
-        
-        return answer;
+        myString = myString.toLowerCase();
+        myString = myString.replace('a','A');
+        return myString;
     }
 }
--- 프로그래머스/0/181874. A 강조하기/README.md
+++ 프로그래머스/0/181874. A 강조하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] A 강조하기 - 181874 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181874?language=java) 
 
 ### 성능 요약
 
-메모리: 92.6 MB, 시간: 0.06 ms
+메모리: 81.7 MB, 시간: 0.03 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 08일 11:38:41
+2025년 07월 08일 11:45:55
 
 ### 문제 설명
 
