e4dd8112be2916ad9456b79a438fe5a30bd84b04
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-09 13:31:49 +0900

    [level 0] Title: 문자열 바꿔서 찾기, Time: 2.21 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
+++ 프로그래머스/0/181864. 문자열 바꿔서 찾기/문자열 바꿔서 찾기.java
@@ -1,11 +1,12 @@
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
+
 class Solution {
     public int solution(String myString, String pat) {
-        // BAABB
-        myString = myString.replace("A","b");
-        myString = myString.replace("B","A");
-        myString = myString.toUpperCase();
-        System.out.println(myString);
-        
-        return myString.contains(pat) ? 1 : 0 ;
+       //기존 BAABB 와 AABB 비교
+        //역 ABBAA 와 BBAA 비교
+        return myString.contains(Stream.of(pat.split(""))
+                                 .map(str -> "A".equals(str)?"B":"A").collect(Collectors.joining()))? 1:0 ;
+            //String -> String[] -> Stream<String> ->String
     }
 }
