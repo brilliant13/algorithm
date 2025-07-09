16d4e35821588dc8e52922597f03aead63b047f7
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-09 13:54:22 +0900

    [level 0] Title: 정수 찾기, Time: 2.78 ms, Memory: 87 MB -BaekjoonHub

--- 프로그래머스/0/181840. 정수 찾기/README.md
+++ 프로그래머스/0/181840. 정수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 정수 찾기 - 181840 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181840) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181840?language=java) 
 
 ### 성능 요약
 
-메모리: 79.4 MB, 시간: 0.02 ms
+메모리: 87 MB, 시간: 2.78 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 09일 13:47:35
+2025년 07월 09일 13:54:20
 
 ### 문제 설명
 
--- 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.java
@@ -1,10 +1,9 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list, int n) {
         //int[] -> IntStream ->
-        // return Arrays.stream(num_list).map(i->i==n?)
-        for(int i=0 ;i<num_list.length; i++){
-            if(num_list[i]==n) return 1;
-        }
-        return 0;
+        return Arrays.stream(num_list).filter(i -> i==n)
+                                      .count() >0 ?1 : 0;
     }
 }
