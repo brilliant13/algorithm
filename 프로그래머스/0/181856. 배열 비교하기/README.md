1bfaf84b40da264ef0a55d2a4cd90aa2a5c36d67
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-16 15:09:13 +0900

    [level 0] Title: 배열 비교하기, Time: 1.06 ms, Memory: 72.4 MB -BaekjoonHub

--- 프로그래머스/0/181856. 배열 비교하기/README.md
+++ 프로그래머스/0/181856. 배열 비교하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 비교하기 - 181856 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181856?language=java) 
 
 ### 성능 요약
 
-메모리: 89.5 MB, 시간: 5.61 ms
+메모리: 72.4 MB, 시간: 1.06 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 16일 15:04:02
+2025년 07월 16일 15:09:09
 
 ### 문제 설명
 
--- 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
+++ 프로그래머스/0/181856. 배열 비교하기/배열 비교하기.java
@@ -1,15 +1,11 @@
-import java.util.Arrays;
+import java.util.stream.*;
 
 class Solution {
     public int solution(int[] arr1, int[] arr2) {
-        if(arr1.length != arr2.length)
-            return arr1.length > arr2.length ? 1 : -1;
-        if(arr1.length == arr2.length){
-            int sum1 = Arrays.stream(arr1).sum();
-            int sum2 = Arrays.stream(arr2).sum();
-            return sum1==sum2 ? 0 : (sum1> sum2 ? 1 : -1) ;
-            // return sum1>sum2 ? 1 : -1;
+        int answer = Integer.compare(arr1.length, arr2.length);
+        if(answer == 0 ){
+            answer = Integer.compare(IntStream.of(arr1).sum(), IntStream.of(arr2).sum());
         }
-        return 0;
+        return answer;
     }
 }
