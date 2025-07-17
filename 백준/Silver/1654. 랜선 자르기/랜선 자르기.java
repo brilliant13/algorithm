11778b0698aa698a16bedd9d20a23d2ac90ddc9e
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-17 19:59:51 +0900

    [Silver II] Title: 랜선 자르기, Time: 168 ms, Memory: 17732 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,38 +1,51 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int K = Integer.parseInt(st.nextToken());
         int N = Integer.parseInt(st.nextToken());
-        long[] cable = new long[K];
-        //K개 랜선 입력
+        //802 743 457 539 정수범위 초과할 수도 있음
+
+        long[] arr = new long[K];
         for (int i = 0; i < K; i++) {
-            cable[i] = Long.parseLong(br.readLine());
+            arr[i] = Long.parseLong(br.readLine());
         }
-        long low = 1;
-        long high = Arrays.stream(cable).max().getAsLong();
-        long answer = 0;
 
-        //이분탐색, 이진탐색
-        while (low <= high) {
+        // 457 539 743 802
+        long low =1;
+        //LongStream -> Long
+        long high = Arrays.stream(arr).max().getAsLong();
+        long result = 0;
 
-            long mid = (low + high) / 2;
-            long pieces = 0;
-            for (long len : cable) pieces += len / mid;
+        while(low<=high){
 
-            if (pieces >= N) {
-                answer = mid;
-                low = mid + 1;
-            } else {
-                high = mid - 1;
+            long mid =(low+high)/2;
+            long total = 0;
+            //mid 길이로 몇 개의 랜선인지
+            for (int i = 0; i < K; i++) {
+                total += (arr[i]/mid);
+            }
+
+            if(total>=N) {
+                result = mid; //현재 길이 저장
+                low = mid+1; //더 긴 길이 시도
+            }else if(total<N){
+                high = mid-1; // 더 짧은 길이 시도
             }
         }
-        System.out.println(answer);
-    }
-}
+        System.out.println(result);
 
+    }
+
+
+
+
+
+}
