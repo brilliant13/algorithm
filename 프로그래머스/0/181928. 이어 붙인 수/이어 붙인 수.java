73cc685114556519e69032f4fa9de24a0ea0c84e
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-18 17:41:25 +0900

    [level 0] Title: 이어 붙인 수, Time: 4.39 ms, Memory: 95.9 MB -BaekjoonHub

--- 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
+++ 프로그래머스/0/181928. 이어 붙인 수/이어 붙인 수.java
@@ -1,17 +1,14 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
 class Solution {
     public int solution(int[] num_list) {
-        String odds = "";
-        String evens = "";
-        
-        for(int val : num_list){
-            if(val%2!=0) {
-                odds += Integer.toString(val);
-            }
-            else {
-                evens += String.valueOf(val);
-            }
-        }
-        
-        return Integer.parseInt(odds)+Integer.parseInt(evens);
+        //int[] => IntStream -> Stream<String>->String
+       String odd = Arrays.stream(num_list).filter(x -> x%2 != 0)
+           .mapToObj(String::valueOf)
+           .collect(Collectors.joining());
+  //int[] -> IntStream -> Stream<String> -> String
+        String even = Arrays.stream(num_list).filter(x-> x%2 ==0)
+            .mapToObj(String::valueOf).collect(Collectors.joining());
+    return Integer.parseInt(odd) + Integer.parseInt(even);        
     }
 }
