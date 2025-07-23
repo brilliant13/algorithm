e535e0c372c8deee453e53ee1971167b298454fe
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-23 16:37:17 +0900

    [level 0] Title: 1로 만들기, Time: 1.77 ms, Memory: 89.3 MB -BaekjoonHub

--- 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -1,14 +1,8 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] num_list) {
-        int count = 0;
-        for(int i =0; i<num_list.length; i++){
-            int num = num_list[i];
-            while(num!=1){
-                if(num%2==0) num /= 2;
-                else num = (num-1)/2;
-                count++;
-            }
-        }
-        return count;
+        return Arrays.stream(num_list)
+            .map(i->Integer.toBinaryString(i).length()-1).sum();
     }
 }
