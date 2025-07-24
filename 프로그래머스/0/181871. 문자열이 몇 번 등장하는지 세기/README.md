468aed0d29261d0d80054d7ca76a8bdd8e23c3a7
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-24 11:54:46 +0900

    [level 0] Title: 문자열이 몇 번 등장하는지 세기, Time: 3.85 ms, Memory: 82 MB -BaekjoonHub

--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 문자열이 몇 번 등장하는지 세기 - 181871 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181871?language=java) 
 
 ### 성능 요약
 
-메모리: 82.6 MB, 시간: 0.31 ms
+메모리: 82 MB, 시간: 3.85 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 24일 11:45:21
+2025년 07월 24일 11:54:42
 
 ### 문제 설명
 
--- 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
+++ 프로그래머스/0/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.java
@@ -1,16 +1,7 @@
+import java.util.stream.IntStream;
 class Solution {
     public int solution(String myString, String pat) {
-        int count =0;
-        while(true){
-            int first = myString.indexOf(pat);//인덱스가 없으면 -1 반환하나?
-            if(first>=0){
-                count++;
-                myString = myString.substring(first+1);
-            }    
-            else{
-                break;
-            }
-        }
-        return count;
+        return (int)IntStream.range(0,myString.length()).filter(i->myString.substring(i).startsWith(pat)).count();
+        
     }
 }
