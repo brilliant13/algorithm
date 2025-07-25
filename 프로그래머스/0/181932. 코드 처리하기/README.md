222076471ce6263c3c90c0900228e98086137ad4
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-25 13:49:01 +0900

    [level 0] Title: 코드 처리하기, Time: 11.71 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/0/181932. 코드 처리하기/README.md
+++ 프로그래머스/0/181932. 코드 처리하기/README.md
@@ -1,10 +1,10 @@
 # [level 0] 코드 처리하기 - 181932 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181932?language=java) 
 
 ### 성능 요약
 
-메모리: 385 MB, 시간: 340.65 ms
+메모리: 81.5 MB, 시간: 11.71 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 25일 13:45:35
+2025년 07월 25일 13:48:59
 
 ### 문제 설명
 
--- 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
+++ 프로그래머스/0/181932. 코드 처리하기/코드 처리하기.java
@@ -1,31 +1,17 @@
 class Solution {
     public String solution(String code) {
-        int mode = 0;
-        String ret = "";
-        // String ret ="EMPTY"
-        char [] codes = code.toCharArray();
-        for(int i=0; i<codes.length; i++){
-            if(mode==0){
-                if(codes[i]=='1'){
-                    mode=1;
-                    continue;
-                }
-                if(i%2==0){
-                    ret +=codes[i];
-                }
-            }else{
-                if(codes[i]=='1'){
-                    mode=0;
-                    continue;
-                }
-                if(i%2!=0){
-                    ret +=codes[i];
-                }
+        StringBuilder answer = new StringBuilder();
+        int mode =0;
+        for(int i=0; i<code.length(); i++){
+            char current = code.charAt(i);
+            if (current =='1'){
+                mode = mode == 0 ? 1 : 0;
+                continue;
+            }
+            if(i%2==mode){
+                answer.append(current);
             }
         }
-        if(ret.equals("")) ret = "EMPTY";
-        
-        return ret;
-        
+        return answer.length() == 0 ? "EMPTY" : answer.toString();
     }
 }
