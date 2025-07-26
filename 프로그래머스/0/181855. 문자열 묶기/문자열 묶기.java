5e95bbfb175262d59539a8ad800e348e6e6aeea6
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-07-26 13:49:02 +0900

    [level 0] Title: 문자열 묶기, Time: 8.95 ms, Memory: 127 MB -BaekjoonHub

---
+++ 프로그래머스/0/181855. 문자열 묶기/문자열 묶기.java
@@ -0,0 +1,19 @@
+import java.util.Arrays;
+
+class Solution {
+    public int solution(String[] strArr) {
+        //문자열 배열 strArr
+        int [] groupCount = new int [31]; //1~30
+        
+        int max = 0;
+        
+        for(int i=0; i<strArr.length; i++){
+            int count = strArr[i].length();
+            groupCount[count]++;
+        }
+        //int[]에서 최댓값 찾는 법 
+        //int[] -> Stream<Integer> ->
+        // return Arrays.stream(groupCount).mapToInt(Integer::intValue).max().getAsInt;
+        return Arrays.stream(groupCount).max().orElse(-1);
+    }
+}
