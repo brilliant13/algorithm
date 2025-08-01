78ceb82b431a804a4dd6a63233ff5ef42f120037
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-02 01:35:05 +0900

    [level 2] Title: 소수 찾기, Time: 25.04 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -3,25 +3,23 @@ import java.util.HashSet;
 
 class Solution {
     public int solution(String numbers) {
-        Set<Integer> primes = new HashSet<>();
+        Set<Integer> primes = new HashSet<>(); //중복x
         char[] nums = numbers.toCharArray();
         boolean[] used = new boolean[nums.length];
         
         //모든 가능한 순열을 생성 -> 소수찾기
-        generateNumbers(nums, used, "", primes);
+        generateNumbers(nums,used,"",primes);
         return primes.size();
-
     }
-    
-    private void generateNumbers(char[] nums, boolean[] used, String current, Set<Integer> primes){
+    public void generateNumbers(char[] nums, boolean[]used, String current, Set<Integer> primes){
         if(!current.isEmpty()){
             int num = Integer.parseInt(current);
-            if (isPrime(num)){
+            if(isPrime(num)){
                 primes.add(num);
             }
         }
         
-        for(int i =0; i<nums.length;i++){
+        for(int i=0; i<nums.length; i++){
             if(!used[i]){
                 used[i] = true;
                 generateNumbers(nums,used,current+nums[i],primes);
@@ -31,10 +29,12 @@ class Solution {
     }
     public boolean isPrime(int n){
         if(n<2) return false;
-        int limit = (int)Math.sqrt(n);
-        for(int d =2; d<= limit; d++){
-            if(n%d == 0) return false;
+        if(n==2) return true;
+        if(n%2==0) return false;
+        for(int i=3; i*i<=n; i+=2){
+            if(n%i == 0) return false;
         }
         return true;
     }
+
 }
