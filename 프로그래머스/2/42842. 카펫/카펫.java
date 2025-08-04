22f8a122f653a6f34ab76974d06223ce5c57084c
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-04 12:36:57 +0900

    [level 2] Title: 카펫, Time: 9.27 ms, Memory: 73.8 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int x =0, y=0;
+        int width =0, height =0;
+        
+        for(int i=yellow; i>=Math.sqrt(yellow); i--){
+            //약수가 아니면 pass
+            if(yellow%i!=0) continue;
+            
+            //약수이면
+            x = i+2;
+            y = (yellow/i) +2;
+            
+            //조건1 (2x+2y)-4 = brown
+            if(2*(x+y) != brown + 4 ) continue;
+            
+            //조건2 (x>=y)
+            if(x<y) continue;
+            
+            width = x;
+            height = y;
+            
+        }
+        int[] answer = {width,height};
+        return answer;
+    }
+}
