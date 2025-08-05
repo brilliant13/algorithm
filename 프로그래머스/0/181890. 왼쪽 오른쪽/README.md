f58eeae82079cb925d70c41a14a601727679d496
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-05 14:58:57 +0900

    [level 0] Title: 왼쪽 오른쪽, Time: 0.04 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/README.md
@@ -1,10 +1,10 @@
 # [level 0] 왼쪽 오른쪽 - 181890 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181890?language=java) 
 
 ### 성능 요약
 
-메모리: 84.6 MB, 시간: 0.04 ms
+메모리: 77 MB, 시간: 0.04 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 05일 14:55:55
+2025년 08월 05일 14:58:55
 
 ### 문제 설명
 
--- 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
+++ 프로그래머스/0/181890. 왼쪽 오른쪽/왼쪽 오른쪽.java
@@ -2,37 +2,17 @@ import java.util.Arrays;
 
 class Solution {
     public String[] solution(String[] str_list) {
-        //String[] -> String 
-        //String[] -> Stream<String> -> collect(Collectors.joingning())
-        //String ->String[] // String.split("") ->String[]반환
-        int idx_l = -1;
-        int idx_r = -1;
         
-        for(int i =0; i<str_list.length; i++){
-            if(str_list[i].equals("l")){
-                idx_l = idx_l == -1 ? i : idx_l;
-            }       
-            if(str_list[i].equals("r")){
-                idx_r = idx_r == -1 ? i : idx_r;
-            }   
+        for(int i=0; i<str_list.length; i++){
+            
+            if("l".equals(str_list[i])){
+                return Arrays.copyOfRange(str_list,0,i);
+            } else if("r".equals(str_list[i])){
+                return Arrays.copyOfRange(str_list,i+1,str_list.length);
+            }
         }
-        
-        // System.out.println(idx_l)
-        ;
-        
-        String answer ="";
-        if(idx_l==-1 && idx_r==-1) return new String[]{};
-        
-        //l만 있거나 (l먼저)
-        if(idx_l != -1 && (idx_r == -1 || idx_l < idx_r)){
-            return Arrays.copyOfRange(str_list,0,idx_l);
-        }
-        
-        //r만 있거나 (r먼저)
-        if(idx_r != -1 && (idx_l == -1 || idx_r < idx_l)){
-            return Arrays.copyOfRange(str_list,idx_r+1,str_list.length);
-        }
-    
         return new String[0];
+    
+        
     }
 }
