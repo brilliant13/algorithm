10bc6a485ba5446052d8f848570b74ed3e9f6bb2
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-05 16:14:32 +0900

    [level 2] Title: 가장 큰 수, Time: 189.75 ms, Memory: 133 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,18 +1,22 @@
-import java.util.stream.IntStream;
-// import java.util.Collections;
+import java.util.Arrays;
 import java.util.stream.Collectors;
-import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        
-        String answer = Arrays.stream(numbers)  // int[] -> IntStream
-                              .mapToObj(String::valueOf) //IntSteram->Stream<String>
-                              .sorted((a,b)->(b+a).compareTo(a+b)) //Stream<String> 내림차순 정렬됨
-                              .collect(Collectors.joining()); //Stream<String> -> String
-        
+        //int[] -> IntStream -> Stream<String> -> String
+        String answer = Arrays.stream(numbers).mapToObj(String::valueOf)
+            .sorted((a,b)->(b+a).compareTo(a+b))
+            .collect(Collectors.joining());
         
         return answer.charAt(0) == '0' ? "0" : answer;
+
+        // String answer = Arrays.stream(numbers)  // int[] -> IntStream
+        //                       .mapToObj(String::valueOf) //IntSteram->Stream<String>
+        //                       .sorted((a,b)->(b+a).compareTo(a+b)) //Stream<String> 내림차순 정렬됨
+        //                       .collect(Collectors.joining()); //Stream<String> -> String
+        
+        
+        // return answer.charAt(0) == '0' ? "0" : answer;
 
     }
 }
