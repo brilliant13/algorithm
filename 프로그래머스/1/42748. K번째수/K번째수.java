616b29d6fa5d30d7f90771516868cc2eed672442
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-05 15:08:46 +0900

    [level 1] Title: K번째수, Time: 0.47 ms, Memory: 82.4 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -2,23 +2,20 @@ import java.util.Arrays;
 
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
-
-        int [] answer = new int[commands.length]; 
         
+        int [] answer = new int[commands.length];
         for(int i=0; i<commands.length; i++){
             int from = commands[i][0];
             int to = commands[i][1];
             int k = commands[i][2];
             
             int size = to-from+1;
-            int [] copy = new int[size];
-            
+            int[] copy = new int[size];
             System.arraycopy(array,from-1,copy,0,size);
             Arrays.sort(copy);
             answer[i] = copy[k-1];
         }
-        
-        //int[]
+
         return answer;
     }
 }
