e156951b1bab3b39b51b47a129b9f1041bfb77ec
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-11 17:30:52 +0900

    [level 0] Title: 배열 만들기 4, Time: 67.49 ms, Memory: 81.6 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/README.md
+++ 프로그래머스/0/181918. 배열 만들기 4/README.md
@@ -1,10 +1,10 @@
 # [level 0] 배열 만들기 4 - 181918 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181918) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/181918?language=java) 
 
 ### 성능 요약
 
-메모리: 80.1 MB, 시간: 23.85 ms
+메모리: 81.6 MB, 시간: 67.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 11일 17:28:35
+2025년 08월 11일 17:30:50
 
 ### 문제 설명
 
--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -1,24 +1,18 @@
-import java.util.List;
-import java.util.ArrayList;
+import java.util.*;
 
 class Solution {
-    public int[] solution(int[] arr) {
+    public Stack<Integer> solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
         int i = 0;
-        //ArrayList로 add,remove하다가 마지막에 int[]로 해주자.
-        List<Integer> list = new ArrayList<>();
-        while(i<arr.length){
-            if(list.size()==0){
-                list.add(arr[i++]);
-            }
-            else if(list.get(list.size()-1)<arr[i]){
-                list.add(arr[i++]);
-            }
-            else {
-                list.remove(list.size()-1);
+
+        while (i < arr.length) {
+            if (stack.empty() || stack.peek() < arr[i]) {
+                stack.push(arr[i]);
+                i++;
+            } else if (stack.peek() >= arr[i]) {
+                stack.pop();
             }
         }
-        //ArrayList<Integer> -> int[]
-        //List<Integer> -> Stream<Integer> -> IntStream -> int[]
-        return list.stream().mapToInt(Integer::intValue).toArray();
+        return stack;
     }
 }
