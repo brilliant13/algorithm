984583f49119dc4eb768f0f9dc23f42bc18cd880
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-12 14:12:42 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 108 ms, Memory: 14372 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -1,23 +1,24 @@
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        final int MOD = 10007;
+
+        final int r = 10007;
+        if(n==1){
+            System.out.println(1); return;
+        }
         int[] dp = new int[n + 1]; //1~n
-        int r = 10007;
         dp[1] = 1;
-        if (n >= 2) {
         dp[2] = 3;
-        }
-        if (n >= 3) {
-        dp[3] = 5;
-        }
-        if (n > 3) {
-        for (int i = 4; i <= n; i++) {
-            dp[i] = (dp[i - 1]%r + 2 * dp[i - 2]%r)%r;
-        }
+
+        for (int i = 3; i <= n; i++) {
+            dp[i] = (dp[i-1] + 2*dp[i-2])%MOD;
         }
         System.out.println(dp[n]);
 
