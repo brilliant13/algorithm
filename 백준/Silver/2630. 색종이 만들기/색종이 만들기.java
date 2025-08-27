0ff01a857f6aa821106ff46531aab759c71dd1e5
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-27 13:54:32 +0900

    [Silver II] Title: 색종이 만들기, Time: 164 ms, Memory: 17336 KB -BaekjoonHub

--- 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
+++ 백준/Silver/2630. 색종이 만들기/색종이 만들기.java
@@ -11,51 +11,37 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[][] arr = new int[N][N];
-        StringTokenizer st;
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
                 arr[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        check_full(arr);
+        solve(arr, 0, 0, N);
         System.out.println(white + "\n" + blue);
     }
 
-    static void check_full(int[][] arr) {
-        int n = arr.length;
-        int m = arr[0].length;
-
-        if (arr.length == 1 && arr[0].length == 1) {
-            if (arr[0][0] == 1) blue++;
-            else white++;
+    static void solve(int[][] a, int r0, int c0, int n) {
+        if (n == 1) {
+            if(a[r0][c0]==1) blue++; else white++;
             return;
         }
-        int first = arr[0][0];
-
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (arr[i][j] != first) {
-                    int mid = n/2;
-                    //재귀
-                    check_full(submatrix(arr, 0, 0, mid, mid));
-                    check_full(submatrix(arr, 0, mid, mid, mid));
-                    check_full(submatrix(arr, mid, 0, mid, mid));
-                    check_full(submatrix(arr, mid, mid, mid, mid));
-                    return;
-                }
+        int first = a[r0][c0];
+        boolean same = true;
+        outer:
+        for (int i = r0; i < r0 + n; i++) {
+            for (int j = c0; j < c0 + n; j++) {
+                if(a[i][j] != first){same = false; break outer;}
             }
         }
-        if(first==1) blue++;
-        else white++;
-
-    }
-
-    static int[][] submatrix(int[][] arr, int r0, int c0, int h, int w) {
-        int[][] res = new int[h][w];
-        for (int r = 0; r < h; r++) {
-            System.arraycopy(arr[r0 + r], c0, res[r], 0, w);
+        if (!same) {
+            int h = n/2;
+            solve(a,r0,c0,h);
+            solve(a, r0, c0 + h, h);
+            solve(a, r0 + h, c0, h);
+            solve(a, r0 + h, c0 + h, h);
+            return;
         }
-        return res;
+        if(first==1) blue++; else white++;
     }
 }
