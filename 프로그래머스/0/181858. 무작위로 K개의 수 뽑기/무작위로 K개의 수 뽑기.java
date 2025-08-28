eda84ebd4d126b0de2e2eebea33ee205d2a9a5eb
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-08-28 11:52:02 +0900

    [level 0] Title: 무작위로 K개의 수 뽑기, Time: 16.13 ms, Memory: 82.9 MB -BaekjoonHub

--- 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
+++ 프로그래머스/0/181858. 무작위로 K개의 수 뽑기/무작위로 K개의 수 뽑기.java
@@ -1,28 +1,11 @@
-import java.util.List;
-import java.util.ArrayList;
-
+import java.util.Arrays;
+import java.util.stream.IntStream;
 class Solution {
     public int[] solution(int[] arr, int k) {
-        boolean[] chk = new boolean[100001];
-        List<Integer> list = new ArrayList<>();
-        
-        int count = 0;
-        for(int num : arr){
-            if(!chk[num]){
-                list.add(num);
-                chk[num] = true;
-                count++;
-            }
-            if(count==k) break;
-        }
-        // System.out.println("list = "+list.size());
-        int size = list.size();
-        if(size<k){
-            for(int i=0; i<k-size; i++){
-                // System.out.println("i = "+i);
-                list.add(-1);
-            }
-        }
-       return list.stream().mapToInt(Integer::intValue).toArray();
+        return IntStream.concat(
+        Arrays.stream(arr).distinct(),
+        IntStream.range(0,k).map(i->-1))
+            .limit(k)
+            .toArray();
     }
 }
