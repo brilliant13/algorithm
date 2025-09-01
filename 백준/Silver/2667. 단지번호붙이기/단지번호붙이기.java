269b6af5e86f51fbf33f7fbb2ebb1ed47f671789
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-01 15:20:02 +0900

    [Silver I] Title: 단지번호붙이기, Time: 120 ms, Memory: 14472 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -9,10 +9,10 @@ public class Main {
     static int[] dy = {0, 0, -1, 1};
     static int[][] map;
     static boolean[][] visited;
-    static StringBuilder sb;
-    static int totalCount = 0;
+    static int complexity;
     static int count = 0;
-    static List<Integer> list;
+    static List<Integer> list = new ArrayList<>();
+
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,51 +21,53 @@ public class Main {
         visited = new boolean[N][N];
 
         for (int i = 0; i < N; i++) {
-//            StringTokenizer st = new StringTokenizer(br.readLine(),"");
-            String[] strs = br.readLine().split("");
-            int idx = 0;
-            for (int j = 0; j < strs.length; j++) {
-                map[i][idx++] = Integer.parseInt(strs[j]);
+            String[] str = br.readLine().split("");
+            for (int j = 0; j < N; j++) {
+                map[i][j] = Integer.parseInt(str[j]);
             }
         }
-        sb = new StringBuilder();
-        list = new ArrayList<>();
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                if (map[i][j] == 1 && !visited[i][j]) {
-                    count =0;
+                if (visited[i][j]) continue;
+                else if (!visited[i][j] && map[i][j] == 1) {
+                    count = 0;
                     bfs(i, j);
+                    complexity++;
                     list.add(count);
-                    totalCount++;
                 }
             }
         }
+        System.out.println(complexity);
         list.sort(Comparator.naturalOrder());
-        System.out.println(totalCount);
-        for (int num : list)System.out.println(num);
+        for (int num : list) {
+            System.out.println(num);
+        }
 
     }
 
-    private static void bfs(int i, int j) {
-        //bfs 메소드 안에는 오직 큐를 이용한 탐색(While문)만 있다.
+    static void bfs(int i, int j) {
         Queue<int[]> q = new ArrayDeque<>();
-        visited[i][j] = true;
         q.offer(new int[]{i, j});
 
         while (!q.isEmpty()) {
             int[] cur = q.poll();
-            int x = cur[0], y =cur[1];
+            int x = cur[0], y = cur[1];
+            visited[x][y] = true;
             count++;
+
             for (int d = 0; d < 4; d++) {
                 int nx = x + dx[d], ny = y + dy[d];
-                if(nx<0 || nx>= N || ny <0 || ny>=N) continue;
-                if(visited[nx][ny]) continue;
-                if(map[nx][ny] == 1){
+                if(nx<0 || nx>=N || ny<0 || ny>=N) continue;
+                if(visited[nx][ny])continue;
+                if (!visited[nx][ny] && map[nx][ny] == 1) {
                     visited[nx][ny] = true;
                     q.offer(new int[]{nx, ny});
                 }
             }
+
         }
+
     }
+
 }
