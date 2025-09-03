89356976910d885ed8ce83683db3158d6f1c80da
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-03 18:04:32 +0900

    [Silver I] Title: 트리 순회, Time: 104 ms, Memory: 14136 KB -BaekjoonHub

--- 백준/Silver/1991. 트리 순회/트리 순회.java
+++ 백준/Silver/1991. 트리 순회/트리 순회.java
@@ -1,102 +1,59 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
-
-    static Node root;
-
-    public static void init(char data, char leftData, char rightData) {
-        if (root == null) {
-            root = new Node(data);
-            if (leftData != '.') {
-                root.left = new Node(leftData);
-            }
-            if (rightData != '.') {
-                root.right = new Node(rightData);
-            }
-        } else {
-            searchNode(root, data, leftData, rightData);
-        }
+    static class Node {
+        char data; Node left, right;
+        Node(char d) { data = d; }
     }
 
-    public static void searchNode(Node node, char data, char leftData, char rightData) {
-        if (node == null) return;
-        if (node.data == data) {
-            if (leftData != '.') {
-                node.left = new Node(leftData);
-            }
-            if (rightData != '.') {
-                node.right = new Node(rightData);
-            }
-        } else {
-            searchNode(node.left, data, leftData, rightData);
-            searchNode(node.right, data, leftData, rightData);
-        }
+    static Map<Character, Node> nodes = new HashMap<>();
+    static StringBuilder pre = new StringBuilder();
+    static StringBuilder in  = new StringBuilder();
+    static StringBuilder post= new StringBuilder();
+
+    static Node get(char c) {  // 필요할 때만 생성
+        //HashMap사용
+        return nodes.computeIfAbsent(c, k -> new Node(k));
     }
 
-    public static void preOrder(Node node) { //전위 : root - left - right
-        if (node != null) {
-            System.out.print(node.data);
-//            System.out.print(' ');
-            preOrder(node.left);
-            preOrder(node.right);
-        }
-    }
-
-    public static void inOrder(Node node) { //중위 : left - root - right
-        if (node != null) {
-            inOrder(node.left);
-            System.out.print(node.data);
-//            System.out.print(' ');
-            inOrder(node.right);
-        }
-    }
-
-    public static void postOrder(Node node) { //후위 : left - right - root
-        if (node != null) {
-            postOrder(node.left);
-            postOrder(node.right);
-            System.out.print(node.data);
-//            System.out.print(' ');
-        }
-    }
-
-
-    public static void main(String[] args) throws IOException {
-        //이진 트리를 입력받아 preorder traversal, inorder traversal, postorder traversal 결과 출력 프로그램 작성하라
+    public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        char[] data = new char[3];
-        StringTokenizer st;
 
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine(), " ");
-            int j = 0;
-            while (st.hasMoreTokens()) {
-                data[j++] = st.nextToken().charAt(0);
-            }
-            init(data[0], data[1], data[2]);
-        }
-//        root = (root != null && root.data == 'A') ? root : new Node('A'); // 안전빵 아님(연결 안됨)
-        preOrder(root);
-        System.out.println();
-        inOrder(root);
-        System.out.println();
-        postOrder(root);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            char p = st.nextToken().charAt(0);
+            char l = st.nextToken().charAt(0);
+            char r = st.nextToken().charAt(0);
 
+            Node parent = get(p);
+            parent.left  = (l == '.') ? null : get(l);
+            parent.right = (r == '.') ? null : get(r);
+        }
+
+        Node root = nodes.get('A'); // 문제 조건: 항상 A가 루트
+        preorder(root);  System.out.println(pre);
+        inorder(root);   System.out.println(in);
+        postorder(root); System.out.println(post);
     }
 
-
-}
-
-class Node {
-    char data;
-    Node left;
-    Node right;
-
-    Node(char data) {
-        this.data = data;
+    static void preorder(Node n) {
+        if (n == null) return;
+        pre.append(n.data);
+        preorder(n.left);
+        preorder(n.right);
+    }
+    static void inorder(Node n) {
+        if (n == null) return;
+        inorder(n.left);
+        in.append(n.data);
+        inorder(n.right);
+    }
+    static void postorder(Node n) {
+        if (n == null) return;
+        postorder(n.left);
+        postorder(n.right);
+        post.append(n.data);
     }
 }
