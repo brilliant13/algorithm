1314bb1447b1a8e58ebca61be304a4dedb2307be
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-09 14:31:44 +0900

    [Silver II] Title: 마인크래프트, Time: 684 ms, Memory: 35704 KB -BaekjoonHub

---
+++ 백준/Silver/18111. 마인크래프트/마인크래프트.java
@@ -0,0 +1,59 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+//        int leastTime = Integer.MAX_VALUE;
+//        int height = 0;
+
+        int[][] map = new int[N][M];
+        int minH = 256, maxH =0; //입력 케이스, 실제 최소, 최대
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine(), " ");
+            for (int j = 0; j < M; j++) {
+                int v = Integer.parseInt(st.nextToken());
+                map[i][j] = v;
+                if(v<minH) minH = v;
+                if(v>maxH) maxH = v;
+            }
+        }
+
+        int bestTime = Integer.MAX_VALUE;
+        int bestH = -1;
+
+//        Map<Integer, Integer> timeBox = new HashMap<>();
+
+        for (int h = 0; h <= 256; h++) { //BruteForce
+            long remove =0, add =0;
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < M; j++) {
+                    int v = map[i][j];
+                    if(v>h) remove += (v - h);
+                    else add += (h - v);
+                }
+            }
+            if (add <= B + remove) {
+                int time = (int) (2 * remove + add);
+                if (time < bestTime || (time == bestTime && h > bestH)) {
+                    bestTime = time;
+                    bestH = h;
+                }
+            }
+        }
+
+        System.out.println(bestTime + " " + bestH);
+        
+
+    }
+}
