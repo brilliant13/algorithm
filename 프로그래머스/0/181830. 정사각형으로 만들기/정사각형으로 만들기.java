15356834dec0b0a4568fe5b8e078bc756a93c7b5
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-19 14:43:34 +0900

    [level 0] Title: 정사각형으로 만들기, Time: 0.10 ms, Memory: 81.9 MB -BaekjoonHub

--- 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
+++ 프로그래머스/0/181830. 정사각형으로 만들기/정사각형으로 만들기.java
@@ -1,24 +1,11 @@
-import java.util.Arrays;
-
-class Solution {
-    public int[][] solution(int[][] arr) {
-        //이차원 정수 배열 arr이 주어진다. 
-        int row = arr.length;
-        int col = arr[0].length;
-        
-        if(row>col){
-            int[][]answer = new int[row][row];
-            for(int i=0; i<row; i++){
-                answer[i] = Arrays.copyOf(arr[i],row);
-            }
-            return answer;
-        }else if(row<col){
-            int[][]answer = new int[col][col];
-            for(int i=0; i<row; i++){
-                answer[i] = Arrays.copyOf(arr[i],col);
-            }
-            return answer;
-        }else return arr;
-            
-    }
+class Solution{
+	public int[][] solution(int[][] arr){
+		int max = Math.max(arr.length, arr[0].length);
+		int[][] array = new int[max][max];
+		
+		for(int i=0; i<arr.length; i++){
+			System.arraycopy(arr[i],0,array[i],0,arr[0].length);
+		}
+		return array;
+	}
 }
