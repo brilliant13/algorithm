ec8835ea1e6bc65421f9d6060f969f084dd03bd3
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-23 17:35:16 +0900

    [Silver III] Title: N과 M (1), Time: 216 ms, Memory: 21084 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.java
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -1,37 +1,41 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
-public class Main{
-    public static int[] arr;
-    public static boolean[] visit;
-    
-    public static void main(String[] args){
-        Scanner in = new Scanner(System.in);
-        
-        int N = in.nextInt();
-        int M = in.nextInt();
-        
-        arr = new int[M];
-        visit = new boolean[N];
-        dfs(N,M,0);
+public class Main {
+    static int N,M;
+    static int[] seq;
+    static boolean[] used;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        seq = new int[M];
+        used = new boolean[N + 1]; //1..N
+
+        dfs(0);
+        System.out.println(sb.toString());
     }
-    
-    public static void dfs(int N, int M, int depth){
-        if(depth==M){
-            for (int val : arr){
-               System.out.print(val+" "); 
+
+    static void dfs(int depth) {
+        //base case
+        if (depth == M) { //길이 M 완성 -> 출력
+            for (int i = 0; i < M; i++) {
+                sb.append(seq[i]).append(i + 1 == M ? '\n' : ' ');
             }
-            System.out.println();
             return;
         }
-        
-        for(int i =0; i<N; i++){
-            if(!visit[i]){
-                visit[i]=true;
-                arr[depth]=i+1;
-                dfs(N,M,depth+1);
-                visit[i]=false;
-            }
+        for (int num = 1; num <= N; num++) { //사전순 보장
+            if(used[num]) continue; //중복 방지
+            used[num] = true;
+            seq[depth] = num;
+            dfs(depth+1);
+            used[num] = false; //backtrack
         }
     }
-    
 }
