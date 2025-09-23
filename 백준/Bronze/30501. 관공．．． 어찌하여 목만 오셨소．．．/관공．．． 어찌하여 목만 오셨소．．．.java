f5987202316bbd507f25c60af5c005ac3d42338f
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-23 17:51:19 +0900

    [Bronze IV] Title: 관공... 어찌하여 목만 오셨소..., Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
+++ 백준/Bronze/30501. 관공．．． 어찌하여 목만 오셨소．．．/관공．．． 어찌하여 목만 오셨소．．．.java
@@ -1,21 +1,18 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-        String str ="";
+
         for (int i = 0; i < N; i++) {
-            str = br.readLine();
-            if (str.contains("S")) {
-                System.out.println(str);
-                return;
+            String name = br.readLine();
+            if (name.indexOf('S') >= 0) { // 또는 name.contains("S")
+                System.out.println(name);
+                return; // 유일하므로 바로 종료
             }
         }
     }
-
 }
