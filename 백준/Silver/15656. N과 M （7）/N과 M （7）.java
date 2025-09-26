496541ce210d9bca9cddb098167ac034be92a8bb
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-09-26 13:39:22 +0900

    [Silver III] Title: N과 M (7), Time: 496 ms, Memory: 121656 KB -BaekjoonHub

---
+++ 백준/Silver/15656. N과 M （7）/N과 M （7）.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+    static int N,M;
+    static int[] list,seq;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        seq = new int[M];
+        list = new int[N];
+
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            list[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(list);
+
+        dfs(0);
+        System.out.print(sb);
+    }
+
+    static void dfs(int depth) {
+        if (depth == M) { //중복순열 완성
+            for (int i = 0; i < M; i++) {
+                sb.append(seq[i]).append((i + 1 == M) ? '\n' : ' ');
+            }
+            return;
+        }
+        for (int i = 0; i < N; i++) {
+            seq[depth] = list[i];
+            dfs(depth + 1);
+        }
+    }
+}
