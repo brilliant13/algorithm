082c4b527e18d50dce9c639b676d3dae6f5375c9
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-10-03 22:13:45 +0900

    [level 1] Title: 신고 결과 받기, Time: 5330.67 ms, Memory: 488 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -1,50 +1,22 @@
-import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.ArrayList;
-import java.util.Collections;
-
+import java.util.List;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        // ["muzi", "frodo", "apeach", "neo"]
-        //각각에게 누가 메일 보냈는지 연결시킨다. k번 신고 들어오면, 각 이름과 연결된 리스트 들어가서 그 사람들에게 메일 보낸다. 각각 count++
-        
-        
-        Map<String,Set<String>> reportUsers = new HashMap<>();
-        Map<String,Integer> reportCount = new HashMap<>();
-        
-       
-        // 입력값 처리: ["muzi frodo","apeach frodo","frodo neo","muzi neo","apeach muzi"]
-        for(int i =0; i<report.length; i++){
-            String[] cur = report[i].split(" "); 
-            String user = cur[0];
-            String target = cur[1];
-            
-            reportUsers.computeIfAbsent(target, v-> new HashSet<>());
-            boolean added = reportUsers.get(target).add(user);
-            if(added){
-            reportCount.put(target, reportCount.getOrDefault(target,0) + 1 );
-            }
+        //String[] -> Stream<String> -> ArrayList
+        List<String> list = Arrays.stream(report).distinct().collect(Collectors.toList());
+        HashMap<String,Integer> count = new HashMap<>();
+        for(String s : list){
+            String target = s.split(" ")[1];
+            count.put(target, count.getOrDefault(target,0)+1);
         }
-        
-        //[muzi: a,b,c  frodo: a,c ..]
-        int[] answer = new int[id_list.length]; //0 0 0 0 0
-        
-        for(int i=0; i<id_list.length; i++){
-            String userName = id_list[i];
-            if(reportCount.getOrDefault(userName,0)>=k){
-                // a b c 각각에 +1해줘야지
-                for(String name : reportUsers.getOrDefault(userName,Collections.emptySet())){
-                    // a. 이름 찾아서, 이메일 카운트++
-                    for(int j=0; j<id_list.length; j++){
-                        if(id_list[j].equals(name)){
-                            answer[j]++;
-                        }
-                    }
-                }
-            }
-        }
-        return answer;
+        //map()은 객체 스트림을 다른 타입의 객체스트림으로 바꿔줌. Stream<String>->Stream<Integer> 등으로
+        //String[] -> Stream<String> -> count() 숫자반환 ?
+        return Arrays.stream(id_list).map(_user -> {
+            final String user = _user;
+            List<String> reportList = list.stream().filter(s->s.startsWith(user + " ")).collect(Collectors.toList()); 
+            return reportList.stream().filter(s->count.getOrDefault(s.split(" ")[1],0)>=k).count();      
+        }).mapToInt(Long::intValue).toArray();
     }
 }
