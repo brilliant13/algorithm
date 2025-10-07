916641ccb61c6c4a47265009fa74fa51f252d0e5
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-10-07 22:36:38 +0900

    [level 2] Title: 주차 요금 계산, Time: 11.95 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/README.md
+++ 프로그래머스/2/92341. 주차 요금 계산/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주차 요금 계산 - 92341 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/92341?language=java) 
 
 ### 성능 요약
 
-메모리: 77.4 MB, 시간: 20.02 ms
+메모리: 78 MB, 시간: 11.95 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 07일 22:31:05
+2025년 10월 07일 22:36:36
 
 ### 문제 설명
 
--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,67 +1,58 @@
-import java.util.Map;
-import java.util.HashMap;
-import java.util.List;
-import java.util.ArrayList;
-import java.util.Set;
-import java.util.HashSet;
-import java.util.Collections;
+import java.util.*;
 
 class Solution {
     public int[] solution(int[] fees, String[] records) {
-    
-    //자동차번호 -> 입/출차 시간기록(분)
-    Map<Integer,List<Integer>> carTimeList = new HashMap<>();
-    //자동차번호 -> 주차 요금
-    Map<Integer,Integer> carFeeList = new HashMap<>();
-    //자동차 리스트
-    Set<Integer> carList = new HashSet<>();
-    
-	  for(int i=0; i<records.length;i++){
-		  String[] strs = records[i].split(" ");
-		  int carNumber = Integer.parseInt(strs[1]);
-		  
-		  //자동차 리스트에 추가
-		  carList.add(carNumber);
-		  //입/출차 시간기록(분)
-		  int h = Integer.parseInt(strs[0].substring(0,2));
-		  int m = Integer.parseInt(strs[0].substring(3));
-		  int time = h*60 + m;
-		  carTimeList.computeIfAbsent(carNumber, k-> (new ArrayList<Integer>())).add(time);
-	  }
-	  
-	  //차랑별 소요 시간-> 요금
-	  for(Map.Entry<Integer,List<Integer>> e : carTimeList.entrySet()){
-		  int carNumber = e.getKey();
-		  List<Integer> cur = e.getValue();
-          int total = 0;
-		  
-		//입/출차 시간기록이 홀수개면 마지막에 23:59 추가
-		if(cur.size()%2 !=0){
-		    cur.add(23*60+59);
-	    }
-	    //두 개씩 계산(출차시간-입차시간)
-	    int[] cals = new int[2];
-	    for(int k=0; k<cur.size(); k++){
-		    cals[k%2] = cur.get(k);
-		    if(k%2!=0){
-			    total += cals[1]-cals[0];
-		    }
-	    }
-        if(total<=fees[0]){
-            carFeeList.put(carNumber,fees[1]);
-        }else{
-            int f = total - fees[0];
-            carFeeList.put(carNumber, fees[1]+(int)Math.ceil((double)f/fees[2])*fees[3]);
-        }
-	  }
-    
-	//차량별 요금 출력
-	List<Integer> carLists = new ArrayList<>(carList);
-    Collections.sort(carLists);
-    
-    //StreamAPI 이용해서 int[]반환
-    //List<Integer> -> Stream<Integer> -> Stream<Integer> -> IntStream -> int[]
-    return carLists.stream().map(i->carFeeList.get(i)).mapToInt(Integer::intValue).toArray();
+        int baseTime = fees[0], baseFee = fees[1], unitTime = fees[2], unitFee = fees[3];
 
+        // 차량번호 -> 시각 기록(분)
+        Map<Integer, List<Integer>> carTimeList = new HashMap<>();
+
+        for (String rec : records) {
+            String[] parts = rec.split(" ");
+            int car = Integer.parseInt(parts[1]);
+
+            int h = Integer.parseInt(parts[0].substring(0, 2));
+            int m = Integer.parseInt(parts[0].substring(3));
+            int t = h * 60 + m;
+
+            carTimeList.computeIfAbsent(car, k -> new ArrayList<>()).add(t);
+        }
+
+        // 차량번호 -> 최종 요금
+        Map<Integer, Integer> carFeeList = new HashMap<>();
+
+        for (Map.Entry<Integer, List<Integer>> e : carTimeList.entrySet()) {
+            int car = e.getKey();
+            List<Integer> times = e.getValue();
+
+            // OUT 없으면 23:59 추가
+            if (times.size() % 2 != 0) times.add(23 * 60 + 59);
+
+            // 총 주차시간
+            int total = 0;
+            for (int i = 0; i < times.size(); i += 2) {
+                total += times.get(i + 1) - times.get(i);
+            }
+
+            // 요금 계산 (한 번만)
+            int fee;
+            if (total <= baseTime) {
+                fee = baseFee;
+            } else {
+                int extra = total - baseTime;
+                int units = (extra + unitTime - 1) / unitTime; // ceil(extra/unitTime)
+                fee = baseFee + units * unitFee;
+            }
+            carFeeList.put(car, fee);
+        }
+
+        // 차량번호 오름차순 결과
+        List<Integer> carList = new ArrayList<>(carFeeList.keySet());
+        Collections.sort(carList);
+
+        return carList.stream()
+                      .map(carFeeList::get)
+                      .mapToInt(Integer::intValue)
+                      .toArray();
     }
 }
