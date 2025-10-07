06e5dbe6b05f506e6d56526ab658829ce01c0b28
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-10-07 23:40:30 +0900

    [level 2] Title: 주차 요금 계산, Time: 8.48 ms, Memory: 81.5 MB -BaekjoonHub

--- 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
+++ 프로그래머스/2/92341. 주차 요금 계산/주차 요금 계산.java
@@ -1,58 +1,31 @@
 import java.util.*;
 
 class Solution {
+
+    public int timeToInt(String time) {
+        String temp[] = time.split(":");
+        return Integer.parseInt(temp[0])*60 + Integer.parseInt(temp[1]);
+    }
     public int[] solution(int[] fees, String[] records) {
-        int baseTime = fees[0], baseFee = fees[1], unitTime = fees[2], unitFee = fees[3];
 
-        // 차량번호 -> 시각 기록(분)
-        Map<Integer, List<Integer>> carTimeList = new HashMap<>();
+        TreeMap<String, Integer> map = new TreeMap<>();
 
-        for (String rec : records) {
-            String[] parts = rec.split(" ");
-            int car = Integer.parseInt(parts[1]);
-
-            int h = Integer.parseInt(parts[0].substring(0, 2));
-            int m = Integer.parseInt(parts[0].substring(3));
-            int t = h * 60 + m;
-
-            carTimeList.computeIfAbsent(car, k -> new ArrayList<>()).add(t);
+        for(String record : records) {
+            String temp[] = record.split(" ");
+            int time = temp[2].equals("IN") ? -1 : 1;
+            time *= timeToInt(temp[0]);
+            map.put(temp[1], map.getOrDefault(temp[1], 0) + time);
         }
+        int idx = 0, ans[] = new int[map.size()];
+        for(int time : map.values()) {
+            if(time <= 0) time += 1439;
+            time -= fees[0];
+            int cost = fees[1];
+            if(time > 0)
+                cost += (time%fees[2] == 0 ? time/fees[2] : time/fees[2]+1)*fees[3];
 
-        // 차량번호 -> 최종 요금
-        Map<Integer, Integer> carFeeList = new HashMap<>();
-
-        for (Map.Entry<Integer, List<Integer>> e : carTimeList.entrySet()) {
-            int car = e.getKey();
-            List<Integer> times = e.getValue();
-
-            // OUT 없으면 23:59 추가
-            if (times.size() % 2 != 0) times.add(23 * 60 + 59);
-
-            // 총 주차시간
-            int total = 0;
-            for (int i = 0; i < times.size(); i += 2) {
-                total += times.get(i + 1) - times.get(i);
-            }
-
-            // 요금 계산 (한 번만)
-            int fee;
-            if (total <= baseTime) {
-                fee = baseFee;
-            } else {
-                int extra = total - baseTime;
-                int units = (extra + unitTime - 1) / unitTime; // ceil(extra/unitTime)
-                fee = baseFee + units * unitFee;
-            }
-            carFeeList.put(car, fee);
+            ans[idx++] = cost;
         }
-
-        // 차량번호 오름차순 결과
-        List<Integer> carList = new ArrayList<>(carFeeList.keySet());
-        Collections.sort(carList);
-
-        return carList.stream()
-                      .map(carFeeList::get)
-                      .mapToInt(Integer::intValue)
-                      .toArray();
+        return ans;
     }
 }
