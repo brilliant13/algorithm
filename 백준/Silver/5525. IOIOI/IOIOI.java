3fb3e2be5825379b4108517367f78c379a82cfef
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-10-14 18:18:53 +0900

    [Silver I] Title: IOIOI, Time: 236 ms, Memory: 20100 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/5525. IOIOI/IOIOI.java
+++ 백준/Silver/5525. IOIOI/IOIOI.java
@@ -3,38 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
     static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws IOException {
-        //N = 1 => IOI
-        //S의 길이 = 13
-        //문자열 S = OOIOIOIOIIOII
-
-        //I로 시작하는 인덱스 -> 그 인덱스에서 문자열 길이만큼 잘라서 Pn과 같은지 비교 equals
-        //Pn = 2백만
-        //S = 백만
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int M = Integer.parseInt(br.readLine());
         String S = br.readLine();
-
-        StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append("IO");
-        }
-        sb.append("I");
-        String str = sb.toString();
-        int length = str.length();
         int count = 0;
-
-        //startsWith(string, offset)
-        for (int i = 0; i + length <= S.length(); i++) {
-            if (S.startsWith(str, i)) {
-                count++;
+        int ioi = 0;
+        // "OOIOIOIOIIOII"
+        // 문자열 길이만큼 선형 탐색 O(M)
+        for (int i = 0; i + 2 < M; ) {
+            //"IOI"
+            if (S.charAt(i) == 'I' && S.charAt(i + 1) == 'O' && S.charAt(i + 2) == 'I') {
+//                System.out.println("i = " + i);
+                ioi++;
+                if (ioi >= N) { //IOIIOIIOIOOO N이 1, ioi가 3이면 count가 계속 올라가야함
+                    count++;
+                }
+                i += 2;
+            } else {
+                //패턴깨지면 초기화
+                ioi = 0;
+                i++;
             }
         }
         System.out.println(count);
     }
-
 }
