18f52b42c0b491af46c8e139a106a38701beba36
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-10-22 17:50:57 +0900

    [Silver I] Title: 동전 게임, Time: 92 ms, Memory: 13996 KB -BaekjoonHub

--- 백준/Silver/9079. 동전 게임/동전 게임.java
+++ 백준/Silver/9079. 동전 게임/동전 게임.java
@@ -1,7 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.math.BigInteger;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -9,57 +8,71 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
 
-        //"한 번의 연산" 마스크 만들기
-        //XOR 1은 비트 뒤집기. x ^ 1 = ~x
-        //9비트 정수로 표현
-        int row0 = (1 << 0) | (1 << 1) | (1 << 2); // 0 0000 0111
-        int row1 = (1 << 3) | (1 << 4) | (1 << 5); // 0 0011 1000
-        int row2 = (1 << 6) | (1 << 7) | (1 << 8); // 1 1100 0000
+        //연산 Mask
+        int row0 = (1 << 0) | (1 << 1) | (1 << 2);// 0 1 2 // 000 000 111
+        int row1 = (1 << 3) | (1 << 4) | (1 << 5);// 3 4 5
+        int row2 = (1 << 6) | (1 << 7) | (1 << 8);// 6 7 8
 
         int col0 = (1 << 0) | (1 << 3) | (1 << 6); //0 3 6
-        int col1 = (1 << 1) | (1 << 4) | (1 << 7);//1 4 7
-        int col2 = (1 << 2) | (1 << 5) | (1 << 8);   //2 5 8
+        int col1 = (1 << 1) | (1 << 4) | (1 << 7); //1 4 7
+        int col2 = (1 << 2) | (1 << 5) | (1 << 8); //2 5 8
 
-        int diagMain = (1 << 0) | (1 << 4) | (1 << 8);//0 4 8
-        int diagAnti = (1 << 2) | (1 << 4) | (1 << 6);//2 4 6
+        int diagMain = (1 << 0) | (1 << 4) | (1 << 8); // 0 4 8
+        int diagAnti = (1 << 2) | (1 << 4) | (1 << 6); // 2 4 6
 
-        int[] moves = {row0, row1, row2, col0, col1, col2, diagMain, diagAnti};
+        int moves[] = {row0, row1, row2, col0, col1, col2, diagMain, diagAnti};
+        //0..7 => 8가지. 총 연산 경우의 수는 2^8
+        //비트로 표현 combo = 0000 0000 ~ 1111 1111 : 0~ 2^8-1 : 256개
 
         StringBuilder sb = new StringBuilder();
 
-        //state = 000 000 000
         while (T-- > 0) {
-            int state = 0;
-            for (int r = 0; r < 3; r++) {
+            int state = 0; // 000 000 000
+            // 000 000 000 ~  111 111 111 : 0~ 2^9-1 = 512개
+            int ans = Integer.MAX_VALUE;
+
+            for (int r = 0; r <= 2; r++) {
                 StringTokenizer st = new StringTokenizer(br.readLine());
-                for (int c = 0; c < 3; c++) {
+                for (int c = 0; c <= 2; c++) {
+
                     char ch = st.nextToken().charAt(0);
+                    //T = 1 , H = 0
+                    // 평탄화 idx = r*행의 개수 + c
                     if (ch == 'T') {
                         int idx = r * 3 + c;
                         state |= (1 << idx);
                     }
                 }
+//                System.out.println("State : " + Integer.toString(state, 2));
             }
+            //state  001 110 110
 
-            int ans = Integer.MAX_VALUE;
-
+            //완전탐색. 연산은 총 8개. on/off 가능한 연산은 2^8
+            // 연산 Mask = Combo = 0000 0000 ~ 1111 1111
+            // 0 ~ 2^8-1 = 0 ~ 255 : 256가지의 경우의 수
+            //연산의 순서는 상관없음. 결과를 놓고 전체가 1인지 0인지 비교하는 것이기 때문이다.
             for (int combo = 0; combo < (1 << 8); combo++) {
+                //0 ~ 255(2^8-1)
+                //하나의 조합인 combo가 오면, 이건 연산의 경우의 수니까
+                //어떤 연산이 켜져있는지 확인하고, 그 연산을 적용해서 count해준다.
+                //그 연산 비트가 1로 켜져있으면 bitcount로 세준다.
                 int cur = state;
-                for (int op = 0; op < 8; op++) { //0000 0000 0부터7까지 연산 인덱스
-                    if ((combo & (1 << op)) != 0) { //해당 op가 적용된 조합 //0이 아니면 op번째 연산이 켜져있는것
-                        cur ^= moves[op]; //뒤집기 연산
+                for (int op = 0; op < 8; op++) {
+                    if ((combo & (1 << op)) != 0) { //해당 op번째 연산이 켜져있다!
+                        //current상태에 적용해준다.
+                        //cur = 001 110 110 연산 마스크랑 XOR해주면 뒤집힌다.
+                        cur ^= moves[op];
                     }
-                } //해당 combo 연산 다 적용한 뒤 검사
-                //state 000 000 000  ~ 111 111 111   : 0~ 2^9 -1 : 0~511
+                }//combo 조합에서 켜져있는 전체 연산 조합을 state에 적용시켜주고, 비교한다.
                 if (cur == 0 || cur == (1 << 9) - 1) {
+                    //000 000 000 or 111 111 111
+                    //combo에서 켜진 비트 세준다.
                     ans = Math.min(ans, Integer.bitCount(combo));
                 }
             }
+            //combo 다 돌고  최솟값을 넣어준다.
             sb.append(ans == Integer.MAX_VALUE ? -1 : ans).append('\n');
-
         }
         System.out.print(sb);
-
-
     }
 }
