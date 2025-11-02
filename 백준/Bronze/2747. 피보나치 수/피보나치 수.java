774b6b5a8f0cfd321a52f5af0d98f42c03792349
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-11-03 00:28:44 +0900

    [Bronze II] Title: 피보나치 수, Time: 104 ms, Memory: 14112 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,15 +1,19 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    static int[] memo = new int[46]; // n <= 45
-    public static void main(String[] args) throws Exception {
+    static int [] memo = new int[46];
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        System.out.println(fib(n));
+        System.out.println(fibo(n));
     }
-    static int fib(int n) {
-        if (n <= 1) return n;
-        if (memo[n] != 0) return memo[n];
-        return memo[n] = fib(n - 1) + fib(n - 2);
+
+    static int fibo(int n) {
+        if(n <= 1) return n;
+        if(memo[n] != 0) return memo[n];
+        return memo[n] = fibo(n - 2) + fibo(n - 1);
     }
 }
