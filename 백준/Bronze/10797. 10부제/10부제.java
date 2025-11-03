a9d4f013e2d836dd102eb2f3500602aeb31d5256
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-11-03 22:20:03 +0900

    [Bronze IV] Title: 10부제, Time: 180 ms, Memory: 17724 KB -BaekjoonHub

---
+++ 백준/Bronze/10797. 10부제/10부제.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+    Scanner sc = new Scanner(System.in);
+    int car = sc.nextInt();
+    int count =0;
+    for(int i=0; i<5; i++){
+        if(car == sc.nextInt()){
+            count++;
+        }
+    }
+    System.out.println(count);
+    sc.close();
+}
+}
