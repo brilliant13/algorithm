3d37604b7886f7c8876c1f859abd5c5df6e0ff8b
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-12-04 01:05:57 +0900

    [Silver V] Title: 다리 놓기, Time: 112 ms, Memory: 14324 KB -BaekjoonHub

---
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.java
@@ -0,0 +1,48 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        //재원이는 한 도시의 시장이 되었다. 이 도시에는 도시의 동쪽과 서쪽으로 나누는 큰 일직선 모양의 강이 흐르고 있다.
+        //하지만 재원이는 다리가 없어서 시민들이 강을 건너는데 큰 불편을 겪고 있음을 알고 다리를 짓기로 결심하였다.
+        //강 주변에서 다리를 짓기에 적합한 곳을 사이트라고 한다.
+        //재원이는 강 주변을 면밀히 조사해 본 결과 강의 서쪽에는 N개의 사이트가 있고, 동쪽에는 M개의 사이트가 있다
+        //N<=M
+        //재원이는 서쪽(N)의 사이트와 동쪽(M)의 사이트를 다리로 연결하려고 한다.
+        //(이때 한 사이트에는 최대 한 개의 다리만 연결될 수 있다.) 1:1
+        //재원이는 다리를 최대한 많이 지으려고 하기 때문에 서쪽의 사이트만큼 다리를 지으려고 한다.
+        //다리끼리는 서로 겹쳐질 수 없다고 할 때 다리를 지을 수 있는 경우의 수를 구하는 프로그램을 작성하라.
+
+        //N   <=>   M
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        int T = Integer.parseInt(br.readLine());
+        StringTokenizer st;
+        while (T-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+            int sum = 0;
+            for (int i = N - 1; i <= M - 1; i++) {
+                sum += comb(i, N - 1);
+            }
+            sb.append(sum).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    public static long comb(int n, int r) {
+        if (r<0 || r>n) return 0;
+        if(r>n-r) r = n-r; //nCr == nC(n-r)
+
+        long res = 1;
+        for (int i = 1; i <= r; i++) {
+            res = res *(n-r+i)/i; //항상 나누어 떨어짐. 조합 공식임.
+        }
+        return res;
+    }
+}
