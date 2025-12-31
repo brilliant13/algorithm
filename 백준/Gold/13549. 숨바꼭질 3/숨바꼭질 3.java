5ebde49bb7886eb6588b15c94f300a5f156b4e1a
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2025-12-31 12:58:18 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 128 ms, Memory: 16016 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,65 +1,60 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayDeque;
+import java.util.Arrays;
+import java.util.Deque;
+import java.util.StringTokenizer;
 
 //숨바꼭질 3
 public class Main {
-    static final int MAX = 1000000;
+
+    static final int MAX = 100_000;
     static final int INF = 1_000_000_000;
-
-
-    static int min = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
-
         System.out.println(bfs(N, K));
-
     }
 
     static int bfs(int N, int K) {
-        //수빈이:N, 동생:K
-        // dist[x] = N에서 x까지 최소 시간
-        int[] dist = new int[MAX + 1]; // 1..based
+        int[] dist = new int[MAX + 1]; //1-based. 1~100_000
         Arrays.fill(dist, INF);
+        dist[N] = 0;
 
         Deque<Integer> dq = new ArrayDeque<>();
-        dist[N] = 0;
         dq.offer(N);
 
-
         while (!dq.isEmpty()) {
-            int x = dq.poll(); //dq.pollFirst()
 
-            if (x == K) return dist[x]; //이미 최솟값 도달
-
-            //1.순간이동 x*2 (비용 0)
-            int nx = x * 2;
-            if (nx <= MAX && dist[nx] > dist[x]) {
-                dist[nx] = dist[x];
-                dq.addFirst(nx); //비용 0이므로 앞쪽에
+            int cur = dq.poll();
+            if (cur == K) {
+                return dist[cur];
             }
 
-            //2. x-1 (비용 1)
-            nx = x - 1;
-            if (nx >= 0 && dist[nx] > dist[x] + 1) {
-                dist[nx] = dist[x] + 1;
-                dq.addLast(nx); //비용 1 -> 뒤쪽
+            //순간이동
+            int nx = cur*2;
+            if (nx <= MAX && dist[nx] > dist[cur]) {
+                dist[nx] = dist[cur];
+                dq.addFirst(nx);
             }
 
-            //3. x+1 (비용 1)
-            nx = x + 1;
-            if (nx <= MAX && dist[nx] > dist[x] + 1) {
-                dist[nx] = dist[x] + 1;
+            //X-1
+            nx = cur-1;
+            if (nx >= 0 && dist[nx] > dist[cur]+1) {
+                dist[nx] = dist[cur]+1;
                 dq.addLast(nx);
             }
-
+            //X+1
+            nx = cur +1;
+            if (nx <= MAX && dist[nx] > dist[cur] + 1) {
+                dist[nx] = dist[cur] + 1;
+                dq.addLast(nx);
+            }
         }
         return dist[K];
-
     }
 }
