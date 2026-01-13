ec80fb4bdf2c6127dddd7b01d646108ec3867089
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2026-01-14 01:07:27 +0900

    [Gold II] Title: 트리의 지름, Time: 668 ms, Memory: 98468 KB -BaekjoonHub

--- 백준/Gold/1167. 트리의 지름/트리의 지름.java
+++ 백준/Gold/1167. 트리의 지름/트리의 지름.java
@@ -33,7 +33,8 @@ public class Main {
                 int c = Integer.parseInt(st.nextToken());
                 //무방향 그래프
                 graph[u].add(new int[]{v, c});
-                graph[v].add(new int[]{u, c});
+                //문제 조건에서 간선 다 주니까, 역방향 굳이 안 넣어도 된다
+//                graph[v].add(new int[]{u, c});
             }
         }
 
@@ -52,7 +53,7 @@ public class Main {
     }
 
     static void dfs(int start) {
-        boolean[] visited = new boolean[V + 1]; //1-based
+        boolean[] visited = new boolean[V + 1]; //1-based O(V)공간. 시간은 초기화 상수취급
         Deque<int[]> stack = new ArrayDeque<>();
         stack.push(new int[]{start, 0});
         visited[start] = true;
@@ -61,7 +62,7 @@ public class Main {
         farDist = 0;
 
         while (!stack.isEmpty()) {
-            int[] cur = stack.pop();
+            int[] cur = stack.pop(); //정점마다 pop 한 번 // V번
             int x = cur[0];
             int dist = cur[1];
 
@@ -70,13 +71,21 @@ public class Main {
                 farNode = x;
             }
 
-            for (int[] edge : graph[x]) {
+            for (int[] edge : graph[x]) { // for는 현재 정점 x의 차수(degree(x))만큼 돈다
+                //deg(1)+deg(2)+..+deg(V)
+                //모든 정점 차수 합 = 2E = 간선 하나에 양 끝 정점의 차수에 각각 1씩 기여하니까
+                //전체차수 2E번 만큼 돈다.
                 if (!visited[edge[0]]) {
                     visited[edge[0]]=true;
                     //거리가 누적되어야 되는구나.
                     stack.push(new int[]{edge[0], edge[1]+dist});
                 }
             }
+
+            //O(V) + O(2E) = O(V+2E) => O(V+E)
+            // 2* O(V+E) => O(V+E)
+            //트리라면 E = V-1
+            // O(V+E) -> O(2V-1) -> O(2V) -> O(V) = 선형시간
         }
     }
 }
