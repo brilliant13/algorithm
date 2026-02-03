708e3896af4f7dca893749fd6e1add53d9d4c243
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2026-02-03 23:23:40 +0900

    [Bronze II] Title: Bunnies, Time: 88 ms, Memory: 14024 KB -BaekjoonHub

--- 백준/Bronze/26529. Bunnies/Bunnies.java
+++ 백준/Bronze/26529. Bunnies/Bunnies.java
@@ -12,13 +12,15 @@ public class Main {
         int T = Integer.parseInt(br.readLine()); //0~45
         StringBuilder sb = new StringBuilder();
 
-        Arrays.fill(fibs, -1);
         fibs[0] = 1;
         fibs[1] = 1;
+        for (int i = 2; i <= 45; i++) {
+            fibs[i] = fibs[i - 1] + fibs[i - 2];
+        }
 
         while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
-            sb.append(fib(n)).append('\n');
+            sb.append(fibs[n]).append('\n');
         }
         System.out.print(sb);
     }
@@ -39,5 +41,7 @@ public class Main {
         }
         return fibs[n];
         //top down(재귀 메모이제이션)  or bottom up (dp, 반복문)
+
+
     }
 }
