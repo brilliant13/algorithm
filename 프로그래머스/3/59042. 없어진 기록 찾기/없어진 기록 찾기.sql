a44c96c449b2bfcdf3993b4e1347e7bd0c2b81d3
Woong Jung <140619565+brilliant13@users.noreply.github.com>
2026-04-09 16:14:49 +0900

    [level 3] Title: 없어진 기록 찾기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
+++ 프로그래머스/3/59042. 없어진 기록 찾기/없어진 기록 찾기.sql
@@ -1,10 +1,7 @@
 
-SELECT o.ANIMAL_ID, o.NAME
+
+select o.animal_id, o.name
 from animal_ins i
 right join animal_outs o on i.animal_id = o.animal_id
--- where i.name is null
 where i.animal_id is null
-order by animal_id;
-
-/*
-*/
+order by o.animal_id;
